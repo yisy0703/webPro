7bc88fa8f0c7dd2173455f9ab01254a745ee337f
yisy0703 <yisy0703@naver.com>
2022-11-29 18:07:29 +0900

    java_ch03_op

--- source/01_Java/ch03_operator/src/com/lec/quiz/Quiz4.java
+++ source/01_Java/ch03_operator/src/com/lec/quiz/Quiz4.java
@@ -6,7 +6,7 @@ public class Quiz4 {
 		System.out.print("나이를 입력하세요 ☞ ");
 		Scanner sc = new Scanner(System.in);
 		int age = sc.nextInt();
-		System.out.println((age>=65)? "경로우대":"일반");
+		System.out.println((age>=65)? "경로우대": ((age>=0)? "일반":"떼끼"));
 		sc.close();
 	}
 }
