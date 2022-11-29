3070e83c919fe73effb359f713ed88c11575388f
yisy0703 <yisy0703@naver.com>
2022-11-29 11:42:08 +0900

    java_ch03_op

--- source/01_Java/ch02_variable/src/com/lec/ex/VarEx04.java
+++ source/01_Java/ch02_variable/src/com/lec/ex/VarEx04.java
@@ -1,8 +1,18 @@
 package com.lec.ex;
-
+// 실수는 double을 쓰자. 연산시 유의점
 public class VarEx04 {
 	public static void main(String[] args) {
+		// 변수 선언은 위치 제약이 없어요. 다만 위에 선언할 것을 추천
 		char c = '씨'; //2byte
+		int i = 20; // 4byte
+		long l = 2200000000L; // 8byte;
+		System.out.println("l = " + l);
+		boolean b = true; // 1byte
+		System.out.println("b = " + b);
+		float f = 3.14159265359F; // 4byte
+		double d = 3.14159265359; // 8byte
+		System.out.println("f = " + f);
+		System.out.println("d = " + d);
 		
 	}
 }
