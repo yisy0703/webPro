fff4cfa17b948c998f9bfb210d51df75f8b97cf4
yisy0703 <yisy0703@naver.com>
2022-11-29 15:56:15 +0900

    java_ch03_op

--- source/01_Java/ch03_operator/src/com/lec/ex/Ex05.java
+++ source/01_Java/ch03_operator/src/com/lec/ex/Ex05.java
@@ -3,12 +3,13 @@ package com.lec.ex;
 public class Ex05 {
 	public static void main(String[] args) {
 		int h = 100;
-		String result; // 짝홀여부결과
-		if(h%2 == 0) {
-			result = "짝수입니다";
-		}else {
-			result = "홀수입니다";
-		}
+		String result = (h%2==0)? "짝수입니다":"홀수입니다";
+//		String result; // 짝홀여부결과
+//		if(h%2 == 0) {
+//			result = "짝수입니다";
+//		}else {
+//			result = "홀수입니다";
+//		}
 		System.out.println("결과는 "+result);
 	}
 }
