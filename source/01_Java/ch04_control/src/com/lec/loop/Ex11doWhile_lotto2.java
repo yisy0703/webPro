581dd82e1438fda7ebd96c2a86fd46913505ad2c
yisy0703 <yisy0703@naver.com>
2022-11-30 17:50:11 +0900

    java_ch03_op

--- source/01_Java/ch04_control/src/com/lec/loop/Ex11doWhile_lotto2.java
+++ source/01_Java/ch04_control/src/com/lec/loop/Ex11doWhile_lotto2.java
@@ -1,22 +1,23 @@
 package com.lec.loop;
 import java.util.Scanner;
 // 컴퓨터가 발생한 로또 난수를 맞추기
-public class Ex11doWhile_lotto1 {
+public class Ex11doWhile_lotto2 {
 	public static void main(String[] args) {
 		int lotto, su; // 컴퓨터 로또번화와 사용자로 부터 입력받은 수
+		int min=1, max=45;
 		Scanner scanner = new Scanner(System.in);
-		// 0 <= Math.random() < 1 : 실수
-		// 0 <= Math.random()*45 < 45 : 실수
-		// 1 <= Math.random()*45+1 < 46 : 실수
-		// 1 <=(int)(Math.random()*45+1) < 46 : 정수
 		lotto = (int)(Math.random()*45+1); // 로또번호
 		do {
-			System.out.print("로또 번호 한개를 맞춰 보세요(1~45) : ");
+			System.out.printf("로또 번호 한개를 맞춰 보세요(%d~%d) : ", min, max);
 			su = scanner.nextInt();
-			if(su>lotto) {
-				System.out.println(su+"보다 작은수를 도전하세요");
+			if(su<min || su>max) {
+				System.out.println("주어진 범위를 참조하세요");
+			}else if(su>lotto) {
+				max = su-1;
+				//System.out.println(su+"보다 작은수를 도전하세요");
 			}else if(su<lotto) {
-				System.out.println(su + "보다 큰수를 도전하세요");
+				min = su+1;
+				//System.out.println(su + "보다 큰수를 도전하세요");
 			}
 		}while(su!=lotto);
 		System.out.println("축하축하 맞추셨습니다");
