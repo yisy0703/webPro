488b67e4fa3ebc03403c102eb434715fc5b5e5a4
yisy0703 <yisy0703@naver.com>
2022-11-30 16:17:37 +0900

    java_ch03_op

---
+++ source/01_Java/ch04_control/src/com/lec/loopQuiz/Quiz4_for.java
@@ -0,0 +1,8 @@
+package com.lec.loopQuiz;
+//2*1=2	3*1=3	4*1=4	5*1=5	6*1=6	7*1=7	8*1=8	9*1=9	
+//2*2=4	3*2=6	4*2=8	5*2=10	6*2=12	7*2=14	8*2=16	9*2=18	
+public class Quiz4_for {
+	public static void main(String[] args) {
+		
+	}
+}
