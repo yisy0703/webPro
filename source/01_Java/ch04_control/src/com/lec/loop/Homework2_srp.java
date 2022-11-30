5fdc008613b7afc3678b336f7bb829a0661a9034
yisy0703 <yisy0703@naver.com>
2022-11-30 18:25:09 +0900

    java_ch03_op

--- source/01_Java/ch04_control/src/com/lec/loop/Homework2_srp.java
+++ source/01_Java/ch04_control/src/com/lec/loop/Homework2_srp.java
@@ -6,12 +6,12 @@ public class Homework2_srp {
 		Scanner scanner = new Scanner(System.in);
 		int you, computer;
 		do{
-			System.out.println("가위(0),바위(1),보(2) 중 하나를 선택하세요(종료:-1) ");
+			System.out.println("가위(0),바위(1),보(2) 중 하나를 선택하세요");
 			you = scanner.nextInt();
 			computer = (int)(Math.random()*3);
 			String computerStr = (computer==0) ? "가위" : (computer==1)?"바위":"보자기";
 			String youStr = (you==0) ? "가위" : (you==1)?"바위":"보자기";
-			if(you<-1 || you>2) {
+			if(you<0 || you>2) {
 				System.out.println("가위(0),바위(1),보(2) 중 하나");
 			}else {
 				if( (you+2)%3 == computer ) {
