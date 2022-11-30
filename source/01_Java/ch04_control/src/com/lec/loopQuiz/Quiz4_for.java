cb9925aa1d451f7c0154cf6f60ee4f1d27dd489e
yisy0703 <yisy0703@naver.com>
2022-11-30 16:33:58 +0900

    java_ch03_op

--- source/01_Java/ch04_control/src/com/lec/loopQuiz/Quiz4_for.java
+++ source/01_Java/ch04_control/src/com/lec/loopQuiz/Quiz4_for.java
@@ -3,6 +3,11 @@ package com.lec.loopQuiz;
 //2*2=4	3*2=6	4*2=8	5*2=10	6*2=12	7*2=14	8*2=16	9*2=18	
 public class Quiz4_for {
 	public static void main(String[] args) {
-		
+		for(int i=1 ; i<10 ; i++) {
+			for(int j=2 ; j<10 ; j++) {
+				System.out.printf("%d * %d = %d\t",j, i, j*i);
+			}
+			System.out.println();// °³Çà
+		}
 	}
 }
