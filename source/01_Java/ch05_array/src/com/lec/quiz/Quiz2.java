fb75e8204d998a14fa27258aef3565ce502608a1
yisy0703 <yisy0703@naver.com>
2022-12-01 16:33:15 +0900

    java_ch03_op

--- source/01_Java/ch05_array/src/com/lec/quiz/Quiz2.java
+++ source/01_Java/ch05_array/src/com/lec/quiz/Quiz2.java
@@ -2,7 +2,7 @@ package com.lec.quiz;
 
 public class Quiz2 {
 	public static void main(String[] args) {
-		int[][] arr = {{5, 5, 5, 5, 5}, 
+		int[][] arr = { {5,  5,  5,  5,  5}, 
 						{10, 10, 10, 10, 10}, 
 						{20, 20, 20, 20, 20}, 
 						{30, 30, 30, 30, 30}};
