37c3666da2dbdbdc6a1fea327ce89d435e9bcfdb
yisy0703 <yisy0703@naver.com>
2022-12-02 11:45:59 +0900

    java_ch03_op

--- source/01_Java/ch06_method/src/com/lec/ex/Ex04_voidMethod.java
+++ source/01_Java/ch06_method/src/com/lec/ex/Ex04_voidMethod.java
@@ -2,12 +2,28 @@ package com.lec.ex;
 // return 값이 없는 메소드
 public class Ex04_voidMethod {
 	public static void main(String[] args) {
-		----------------------------
-		Hello, Java
-		~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		Hello, Program
-		============================
-		Hello, HTML
-		■■■■■■■■■■■■
+		printLine(); 
+		System.out.println("Hello, World!");
+		printLine('~'); 
+		System.out.println("Hello, Java");
+		printLine(15);
+	}
+	private static void printLine(int cnt) {
+		for(int i=0 ; i<cnt ; i++) {
+			System.out.print('-');
+		}
+		System.out.println();
+	}
+	private static void printLine(char ch) {
+		for(int i=0 ; i<20 ; i++) {
+			System.out.print(ch);
+		}
+		System.out.println(); // 기본 라인 그리고 개행
+	}
+	private static void printLine() {
+		for(int i=0 ; i<20 ; i++) {
+			System.out.print('-');
+		}
+		System.out.println(); // 기본 라인 그리고 개행
 	}
 }
