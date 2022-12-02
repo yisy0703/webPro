f9b3ef2868290bbcea93720ca5b79c37300d7fef
yisy0703 <yisy0703@naver.com>
2022-12-02 11:20:44 +0900

    java_ch03_op

---
+++ source/01_Java/ch06_method/src/com/lec/ex/Ex04_voidMethod.java
@@ -0,0 +1,13 @@
+package com.lec.ex;
+// return 값이 없는 메소드
+public class Ex04_voidMethod {
+	public static void main(String[] args) {
+		----------------------------
+		Hello, Java
+		~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+		Hello, Program
+		============================
+		Hello, HTML
+		■■■■■■■■■■■■
+	}
+}
