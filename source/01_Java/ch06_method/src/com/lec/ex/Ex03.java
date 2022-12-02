19e55b8fd985ea63f5c4d2fa0f7ab1ce3efd80bb
yisy0703 <yisy0703@naver.com>
2022-12-02 10:59:02 +0900

    java_ch03_op

---
+++ source/01_Java/ch06_method/src/com/lec/ex/Ex03.java
@@ -0,0 +1,9 @@
+package com.lec.ex;
+
+import com.lec.method.Arithmetic;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		System.out.println("-9의 절대값 : " + Arithmetic.abs(-9));
+	}
+}
