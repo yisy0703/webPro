5083e4827c1c7f3fa9080b07d6db7830fae8f0db
yisy0703 <yisy0703@naver.com>
2022-12-02 17:41:13 +0900

    java_ch03_op

--- source/01_Java/ch07_oop/src/com/lec/ex/RectMain.java
+++ source/01_Java/ch07_oop/src/com/lec/ex/RectMain.java
@@ -2,8 +2,6 @@ package com.lec.ex;
 public class RectMain {
 	public static void main(String[] args) {
 		Rect r1 = new Rect();
-		System.out.println("r1 : " +r1.area());
 		Rect r2 = new Rect();
-		System.out.println("r2 : " +r2.area());
 	}
 }
