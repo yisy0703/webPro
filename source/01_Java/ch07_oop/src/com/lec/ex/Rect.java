2e8fdd6af38f54f8d3f4caaa4fb1388218a379ed
yisy0703 <yisy0703@naver.com>
2022-12-02 18:00:19 +0900

    java_ch03_op

--- source/01_Java/ch07_oop/src/com/lec/ex/Rect.java
+++ source/01_Java/ch07_oop/src/com/lec/ex/Rect.java
@@ -3,7 +3,16 @@ package com.lec.ex;
 public class Rect {
 	private int width;
 	private int height;
-	// 생성자가 없을 경우 JVM이 자동 생성
+	// 생성자가 없을 경우 JVM이 자동 생성 
+	public Rect() {} // 디폴트 생성자 함수
+	public Rect(int side) {
+		width = side;
+		height = side;
+	}
+	public Rect(int width, int height) { // 매개변수 2개짜리 생성자 함수
+		this.width = width;
+		this.height = height;
+	}
 	public int area() {
 		return width*height;
 	}
