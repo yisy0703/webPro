0a6ea53048b3dbfe6252dd8a2783859aacc749ba
yisy0703 <yisy0703@naver.com>
2022-12-02 11:12:42 +0900

    java_ch03_op

--- source/01_Java/ch06_method/src/com/lec/ex/Ex03.java
+++ source/01_Java/ch06_method/src/com/lec/ex/Ex03.java
@@ -1,9 +1,14 @@
 package com.lec.ex;
-
 import com.lec.method.Arithmetic;
-
 public class Ex03 {
 	public static void main(String[] args) {
 		System.out.println("-9의 절대값 : " + Arithmetic.abs(-9)); //default접근제한자는 같은 패키지만 사용
+		Arithmetic ar = new Arithmetic();
+		int tot = ar.sum(10);
+		System.out.println("합은 : " + tot);
+		System.out.println(ar.evenOdd(tot));
+		tot = ar.sum(10, 100);
+		System.out.println("합은 " + tot);
+		System.out.println(ar.evenOdd(tot));
 	}
 }
