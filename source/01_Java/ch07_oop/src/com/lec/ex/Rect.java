2a26886fa6813beb4d6d2e3c0af72cb7a9945b34
yisy0703 <yisy0703@naver.com>
2022-12-02 17:38:37 +0900

    java_ch03_op

---
+++ source/01_Java/ch07_oop/src/com/lec/ex/Rect.java
@@ -0,0 +1,24 @@
+package com.lec.ex;
+// 클래스 : 속성, 메소드, setter&getter
+public class Rect {
+	private int width;
+	private int height;
+	// 생성자가 없을 경우 JVM이 자동 생성
+	public int area() {
+		return width*height;
+	}
+	// getter
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	// setter
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+}
