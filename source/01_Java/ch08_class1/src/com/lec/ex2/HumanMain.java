9cf92c7714694d7bbbeaaad5ab9b5d2d429b86bb
yisy0703 <yisy0703@naver.com>
2022-12-05 11:26:33 +0900

    java_ch03_op

--- source/01_Java/ch08_class1/src/com/lec/ex2/HumanMain.java
+++ source/01_Java/ch08_class1/src/com/lec/ex2/HumanMain.java
@@ -1,9 +1,30 @@
 package com.lec.ex2;
-import com.lec.ex2_human.Man;
+// ctrl + shift + o : 불필요한 import 제거
+import com.lec.ex2_human.*;
+import com.lec.ex2_human.kid.Kid;
 public class HumanMain {
 	public static void main(String[] args) {
+		Woman woman1    =  new Woman();
+		Woman woman2    =  new Woman();
+//      ------------       ------------------
+//  레퍼런스변수(객체변수) 선언    객체생성(인스턴스 생성)
+		// System.out.println("woman1과 woman2가 같은지 : " + (woman1 == woman2));
+		System.out.println("woman1과 woman2가 같은지 : " + woman1.equals(woman2));
 		Man man = new Man();
-		Man kim = 
-		new Man("홍길동", 22, 170, 65);
+		Kid kid1 = new Kid("홍아기");
+		// Kid kid2 = new Kid(); 개발자가 생성자 함수를 만들면 디폴트 생성자 함수는 자동 생성 안 함
+		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
