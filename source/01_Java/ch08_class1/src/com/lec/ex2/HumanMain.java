c78ca28af939b60c3e43bc980146625a3f058c0f
yisy0703 <yisy0703@naver.com>
2022-12-05 10:49:37 +0900

    java_ch03_op

---
+++ source/01_Java/ch08_class1/src/com/lec/ex2/HumanMain.java
@@ -0,0 +1,9 @@
+package com.lec.ex2;
+import com.lec.ex2_human.Man;
+public class HumanMain {
+	public static void main(String[] args) {
+		Man man = new Man();
+		Man kim = 
+		new Man("ȫ�浿", 22, 170, 65);
+	}
+}
---
+++ source/01_Java/ch08_class1/src/com/lec/ex2_human/Man.java
@@ -0,0 +1,48 @@
+package com.lec.ex2_human;
+//Man kim = new Man();
+//Man kim = new Man("홍길동");
+// Man kim = new Man("홍길동", 22, 170, 65);
+public class Man {
+	private String name;
+	private int age;
+	private int height;
+	private double weight;
+	// 개발자가 생성자 함수를 만들면 컴파일러는 디폴트 생성자 함수를 안 만들어
+	public Man() {
+		System.out.println("매개변수 없는 생성자 함수");
+	}
+	public Man(String name, int age, int height, double weight) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		System.out.println("데이터 4개 초기화하는 생성자 함수");
+	}
+	// Man man = new Man(170);
+	public Man(String name) {
+		this.name = name;
+		System.out.println("이름 초기화하는 생성자 함수");
+	}
+	public Man(int height) {
+		this.height = height;
+	}
+	public Man(double weight) {
+		this.weight = weight;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
