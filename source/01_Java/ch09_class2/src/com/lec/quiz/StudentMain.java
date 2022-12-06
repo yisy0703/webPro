8d694d969547b0fec74875a517059370efe11c72
yisy0703 <yisy0703@naver.com>
2022-12-06 16:08:17 +0900

    java_ch10

--- source/01_Java/ch09_class2/src/com/lec/quiz/StudentMain.java
+++ source/01_Java/ch09_class2/src/com/lec/quiz/StudentMain.java
@@ -1,7 +1,13 @@
 package com.lec.quiz;
-
+import com.lec.ex1_student.Student;
 public class StudentMain {
 	public static void main(String[] args) {
-		Student s1 = new Student();
+		
+	}
+	private static void line(char c) {
+		for(int cnt=0 ; cnt <55 ; cnt++)
+			System.out.print(c);
+		System.out.println();
+		
 	}
 }
