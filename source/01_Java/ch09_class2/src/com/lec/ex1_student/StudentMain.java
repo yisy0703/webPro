6c55f2649698e6f87b7249a0b5df18c0456855ec
yisy0703 <yisy0703@naver.com>
2022-12-06 10:39:11 +0900

    java_ch10

--- source/01_Java/ch09_class2/src/com/lec/ex1_student/StudentMain.java
+++ source/01_Java/ch09_class2/src/com/lec/ex1_student/StudentMain.java
@@ -1,4 +1,7 @@
 package com.lec.ex1_student;
+
+import java.io.ObjectInputStream.GetField;
+
 public class StudentMain {
 	public static void main(String[] args) {
 		Student s1 = new Student("정우성", 90, 90, 90);
@@ -8,6 +11,12 @@ public class StudentMain {
 		Student s5 = new Student("유아인", 70, 70, 90);
 		Student[] students = {s1, s2, s3, s4, s5};
 		String[] title = {"이름","국어","영어","수학","총점","평균"};
+//		int korTot=0;
+//		int engTot = 0;
+//		int matTot = 0;
+//		int totTot = 0;
+//		int avgTot = 0;
+		int[] total = new int[5];// 0번idx:국어누적, 1번idx:영어누적..4번idx:평균누적
 		line(); 
 		System.out.println("\t\t성적표");
 		line('-');
@@ -19,7 +28,25 @@ public class StudentMain {
 		for(Student student : students) {
 			System.out.print(student.infoString());
 			//student.infoPrint();
+			// 총점변수(국,영,수,총,평) 누적
+			total[0] += student.getKor(); // 국어 누적
+			total[1] += student.getEng(); // 영어 누적
+			total[2] += student.getMat(); // 수학 누적
+			total[3] += student.getTot(); // 총점 누적
+//			total[4] = total[4] + student.getAvg(); // 평균 누적 에러
+			total[4] += student.getAvg(); // 평균 누적
 		}
+		line('-');
+		System.out.print("\t총점");
+		for(int t : total) {
+			System.out.print("\t"+t);
+		}
+		System.out.print("\n\t평균");
+		for(int t : total) {
+			//System.out.print("\t" + t/students.length);
+			System.out.printf("\t%.1f", (double)t/students.length);
+		}
+		System.out.println();//개행
 		line();
 	}
 
