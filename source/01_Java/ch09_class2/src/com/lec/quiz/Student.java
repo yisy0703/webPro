dc09755f2ea07c8ff0b1c524a80bbb606c90f2a3
yisy0703 <yisy0703@naver.com>
2022-12-06 17:52:39 +0900

    java_ch10

--- source/01_Java/ch09_class2/src/com/lec/quiz/Student.java
+++ source/01_Java/ch09_class2/src/com/lec/quiz/Student.java
@@ -1,5 +1,31 @@
 package com.lec.quiz;
 // 객체 생성 : new Student("정우성", 90, 80, 100); -> 번호, 총점, 평균도 같이 초기화
 public class Student {
-	
+	private static int count;
+	private int no;
+	private String name;
+	private int kor;
+	private int eng;
+	private int mat;
+	private int tot;
+	private int avg;
+	public Student(){}
+	public Student(String name, int kor, int eng, int mat) {
+		no = ++count;
+		this.name=name;
+		this.kor=kor;
+		this.eng=eng;
+		this.mat=mat;
+		tot = kor+eng+mat;
+		avg = tot/3;//버림
+		avg = (int)(tot/3.0+0.5); // 반올림
+	}
+	public String infoString() {
+		return no+"\t"+name+"\t"+kor+"\t"+eng+"\t"+mat+"\t"+tot+"\t"+avg;
+	}
+	public int getKor() {return kor;}
+	public int getEng() {return eng;}
+	public int getMat() {return mat;}
+	public int getTot() {return tot;}
+	public int getAvg() {return avg;}
 }
--- source/01_Java/ch09_class2/src/com/lec/quiz/StudentMain.java
+++ source/01_Java/ch09_class2/src/com/lec/quiz/StudentMain.java
@@ -1,8 +1,43 @@
 package com.lec.quiz;
-import com.lec.ex1_student.Student;
+//import com.lec.ex1_student.Student;
 public class StudentMain {
 	public static void main(String[] args) {
-		
+		Student[] students = {new Student("정우성", 90, 91, 91),
+							  new Student("김하늘", 100, 80, 95),
+							  new Student("황정민", 95, 80, 90),
+							  new Student("강동원", 95, 90, 99),
+							  new Student("유아인", 90, 90, 80)};
+		int[] tot = new int[students.length];
+		int[] avg = new int[tot.length];
+		line('■');
+		System.out.println("\t\t\t성 적 표");
+		line('-');
+		System.out.println("번호\t이름\t국어\t영어\t수학\t총점\t평균");
+		line('-');
+		for(Student student : students) {
+			System.out.println(student.infoString());
+			tot[0] += student.getKor();
+			tot[1] += student.getEng();
+			tot[2] += student.getMat();
+			tot[3] += student.getTot();
+			tot[4] += student.getAvg();
+		}
+		line('-');
+		for(int idx=0 ; idx<avg.length ; idx++) {
+			avg[idx] = tot[idx]/students.length; // 버림(86.6은 86)
+			avg[idx] = (int)((double)tot[idx]/students.length+0.5); // 반올림(86.6은 90으로)
+		}
+		System.out.print("총점"+"\t\t");
+		for(int t : tot) {
+			System.out.print(t+"\t");
+		}
+		System.out.println();
+		System.out.print("평균"+"\t\t");
+		for(int a : avg) {
+			System.out.print(a+"\t");
+		}
+		System.out.println();
+		line('■');
 	}
 	private static void line(char c) {
 		for(int cnt=0 ; cnt <55 ; cnt++)
