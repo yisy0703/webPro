2831d49a9cbc905048a475568fcf668aea80a3ad
yisy0703 <yisy0703@naver.com>
2022-12-06 15:59:41 +0900

    java_ch10

---
+++ source/01_Java/ch09_class2/src/com/lec/quiz/Student.java
@@ -0,0 +1,7 @@
+package com.lec.quiz;
+// 객체 생성 : new Student("정우성", 90, 80, 100); -> 번호, 총점, 평균도 같이 초기화
+public class Student {
+	public Student(String name, int kor, int eng, int mat) {
+		
+	}
+}
---
+++ source/01_Java/ch09_class2/src/com/lec/quiz/StudentMain.java
@@ -0,0 +1,7 @@
+package com.lec.quiz;
+
+public class StudentMain {
+	public static void main(String[] args) {
+		Student s1 = new Student();
+	}
+}
