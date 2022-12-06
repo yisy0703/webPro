1ca3c871d382f0df7eb059bd03162cc81cb61aa2
yisy0703 <yisy0703@naver.com>
2022-12-06 12:08:15 +0900

    java_ch10

--- source/01_Java/ch09_class2/src/com/lec/ex2_parking/Parking.java
+++ source/01_Java/ch09_class2/src/com/lec/ex2_parking/Parking.java
@@ -1,5 +1,32 @@
 package com.lec.ex2_parking;
 
 public class Parking {
-	
+	private String no;
+	private int inTime;
+	private int outTime;
+	private int fee;
+	public Parking(String no, int inTime) {
+		this.no = no;
+		this.inTime = inTime;
+		System.out.println(no+"님 어서오세요");
+		System.out.println("입차시간 : "+ inTime +"시");
+		System.out.println("***********************");
+	}
+	public void out(int outTime) {
+		this.outTime = outTime;
+		fee =  (outTime-inTime) * 2000;
+		System.out.println(no+"님 안녕히 가세요");
+		System.out.println("* 입차시간 : "+ inTime +"시");
+		System.out.println("* 출차시간 : "+ outTime +"시");
+		System.out.println("* 주차요금 : "+fee+"원");
+		System.out.println("***********************");
+	}
+	public String getNo() {return no;}
+	public void setNo(String no) {this.no = no;}
+	public int getInTime() {return inTime;}
+	public void setInTime(int inTime) {this.inTime = inTime;}
+	public int getOutTime() {return outTime;}
+	public void setOutTime(int outTime) {this.outTime = outTime;}
+	public int getFee() {return fee;}
+	public void setFee(int fee) {this.fee = fee;}
 }
