ad9e20fe66ce896cf2bae78d1f5966e8c6ed83cd
yisy0703 <yisy0703@naver.com>
2022-12-07 16:32:23 +0900

    java_ch10 inheritance

--- source/01_Java/ch10_inheritance/src/com/lec/ex09_customer/Customer.java
+++ source/01_Java/ch10_inheritance/src/com/lec/ex09_customer/Customer.java
@@ -1,10 +1,28 @@
 package com.lec.ex09_customer;
-
+// name, tel
 public class Customer extends Person {
-
-	public Customer(String name, String tel) {
+	private String address;
+	private int    sum; // 구매금액 누적
+	private int    point; // 포인트(구매시 0.5%)
+	private String date;  // String을 다음주부터는 Date형
+	private boolean vip;
+	// Customer c = new Customer("홍길동", "010-9999-9999", "서울", "12-12")
+	public Customer(String name, String tel, String address, String date) {
 		super(name, tel);
-		// TODO Auto-generated constructor stub
+		this.address = address;
+		this.date = date;
+		point = 1000;
+		System.out.println(name+"님 가입 감사합니다. 포인트 1000점을 드렸습니다");
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
