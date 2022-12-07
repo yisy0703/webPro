46dc41cbcd5fa37b6ee93996b4c1772a9b393742
yisy0703 <yisy0703@naver.com>
2022-12-07 11:05:12 +0900

    java_ch10

--- source/01_Java/ch10_inheritance/src/com/lec/ex03_point/Point.java
+++ source/01_Java/ch10_inheritance/src/com/lec/ex03_point/Point.java
@@ -3,13 +3,16 @@ package com.lec.ex03_point;
 public class Point {
 	private int x;
 	private int y;
-	// 매개변수 없는 생성자 함수는 없음
+	public Point() {
+		System.out.println("매개변수 없는 Point(부모클래스=Parent클래스=Super클래스=상위클래스) 생성자");
+	}
 	public Point(int x, int y) {
+		System.out.println("매개변수 있는 Point 생성자");
 		this.x = x;
 		this.y = y;
 	}
 	public void infoPrint() {
-		System.out.println("2차원 좌표 : x=" + x + ", y="+y);
+		System.out.println("좌표 : x=" + x + ", y="+y);
 	}
 	public int getX() {return x;}
 	public void setX(int x) {this.x = x;}
