8526e5bbb7393c84cc2f6cb6552996b14e0687e8
yisy0703 <yisy0703@naver.com>
2022-12-07 17:00:59 +0900

    java_ch10 inheritance

---
+++ source/01_Java/ch10_inheritance/src/com/lec/ex09_customer/TestMain.java
@@ -0,0 +1,6 @@
+package com.lec.ex09_customer;
+public class TestMain {
+	public static void main(String[] args) {
+		Customer customer = new Customer("홍길동", "010-9999-9999", "서울시 서대문구", "12-01");
+	}
+}
