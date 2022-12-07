d1ed30bd15272814389f8f96799a00f052a10906
yisy0703 <yisy0703@naver.com>
2022-12-07 16:51:15 +0900

    java_ch10 inheritance

--- source/01_Java/ch10_inheritance/src/com/lec/ex09_customer/Customer.java
+++ source/01_Java/ch10_inheritance/src/com/lec/ex09_customer/Customer.java
@@ -1,5 +1,7 @@
 package com.lec.ex09_customer;
-// name, tel
+
+import cons.Constant;
+// name, tel, infoString
 public class Customer extends Person {
 	private String address;
 	private int    sum; // 구매금액 누적
@@ -14,7 +16,23 @@ public class Customer extends Person {
 		point = 1000;
 		System.out.println(name+"님 가입 감사합니다. 포인트 1000점을 드렸습니다");
 	}
-
+	public void buy(int price) { // c.buy(2000);
+		sum += price; // 구매금액에 누적
+		//int tempPoint =  (int)(price * 0.05);
+		int tempPoint = (int)(price * Constant.RATE);
+		point += tempPoint; // 포인트 누적
+		System.out.println(getName()+"님 감사. 금번 포인트 : " + tempPoint);
+		System.out.println("누적 포인트 : " + point);
+		System.out.println("구매금액 : " + price);
+		if(vip==false && sum >= Constant.VIPLIMIT) {
+			vip = true;
+			System.out.println(getName()+"님 VIP고객으로 업그레이드 되셨습니다. 자주 뵙겠습니다");
+		}//if
+	}//buy
+	@Override
+	public String infoString() {
+		return super.infoString() + "   [주소]"+address + "   [포인트]" + point;
+	}
 }
 
 
