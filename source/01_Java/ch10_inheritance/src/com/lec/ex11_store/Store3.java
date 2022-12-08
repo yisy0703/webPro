38e25cff82261b3eab7c28d1ed83711d21f8c002
yisy0703 <yisy0703@naver.com>
2022-12-08 10:31:10 +0900

    java_ch11 abstract

--- source/01_Java/ch10_inheritance/src/com/lec/ex11_store/Store3.java
+++ source/01_Java/ch10_inheritance/src/com/lec/ex11_store/Store3.java
@@ -5,19 +5,11 @@ public class Store3 extends HeadQuarterStore{
 		super(storeName);
 	}
 	@Override
-	public void kimchi() {
-		System.out.println("±èÄ¡Âî°³ : 6,000¿ø");
-	}
+	public void kimchi() {System.out.println("±èÄ¡Âî°³ : 6,000¿ø");}
 	@Override
-	public void bude() {
-		System.out.println("ºÎ´ëÂî°³ : 7,000¿ø");
-	}
+	public void bude() {System.out.println("ºÎ´ëÂî°³ : 7,000¿ø");}
 	@Override
-	public void bibim() {
-		System.out.println("ºñºö¹ä : 7,000¿ø");
-	}
+	public void bibim() {System.out.println("ºñºö¹ä : 7,000¿ø");}
 	@Override
-	public void sunde() {
-		System.out.println("¾óÅ«¼ø´ë±¹ : 6,000¿ø");
-	}
+	public void sunde() {System.out.println("¾óÅ«¼ø´ë±¹ : 6,000¿ø");}
 }
