e420c46ea7c87c4f371e416aff15bdec05d794e3
yisy0703 <yisy0703@naver.com>
2022-12-08 17:38:56 +0900

    java_ch11 abstract

---
+++ source/01_Java/ch11_abstractClass/src/com/lec/quiz/Employee.java
@@ -0,0 +1,20 @@
+package com.lec.quiz;
+public abstract class Employee {
+	private String name;
+	public Employee(String name) {
+		this.name = name; 
+	}
+	public abstract int computePay();
+	public final int computeIncentive(){
+		int temp = computePay();
+		if (temp>3000000) 
+			return (int)(temp*0.05);
+		return 0;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
