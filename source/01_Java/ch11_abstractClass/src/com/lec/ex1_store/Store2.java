623b0a43b82d424c42e8d3f4d68703e18e5e84cb
yisy0703 <yisy0703@naver.com>
2022-12-08 14:38:59 +0900

    java_ch11 abstract

---
+++ source/01_Java/ch11_abstractClass/src/com/lec/ex1_store/Store2.java
@@ -0,0 +1,19 @@
+package com.lec.ex1_store;
+// 2È£Á¡ : ºÎ´ëÂî°³-5,000  ºñºö¹ä-5,000 °ø±â¹ä ¹«·á
+public class Store2 extends HeadQuarterStore {
+	public Store2(String storeName) {
+		super(storeName);
+	}
+	@Override
+	public void bude() {
+		System.out.println("ºÎ´ëÂî°³ : 5,000¿ø");
+	}
+	@Override
+	public void bibim() {
+		System.out.println("ºñºö¹ä : 5,000¿ø");
+	}
+	@Override
+	public void gongibab() {
+		System.out.println("°ø±â¹ä : ¹«·á");
+	}
+}
