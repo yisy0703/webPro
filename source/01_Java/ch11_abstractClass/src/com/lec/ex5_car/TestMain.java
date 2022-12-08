b831b4cf9c9e9d4c5977ef9fc0e0d9ad3a92bd8d
yisy0703 <yisy0703@naver.com>
2022-12-08 17:18:00 +0900

    java_ch11 abstract

--- source/01_Java/ch11_abstractClass/src/com/lec/ex5_car/TestMain.java
+++ source/01_Java/ch11_abstractClass/src/com/lec/ex5_car/TestMain.java
@@ -1,7 +1,12 @@
 package com.lec.ex5_car;
-
+import com.lec.cons.CarSpec;
 public class TestMain {
 	public static void main(String[] args) {
-		Car lowCar = new LowGradeCar("GRAY", "NORMAL", 800, "POWER");
+		Car lowCar = new LowGradeCar(CarSpec.COLOR_GRAY, CarSpec.TIRE_NORMAL, 
+				CarSpec.DISPLACEMENT800, CarSpec.HANDLE_POWER);
+		Car highCar = new HighGradeCar(CarSpec.COLOR_WHITE, CarSpec.TIRE_WIDE, 
+				CarSpec.DISPLACEMENT2000, CarSpec.HANDLE_POWER);
+		lowCar.getSpec();
+		highCar.getSpec();
 	}
 }
