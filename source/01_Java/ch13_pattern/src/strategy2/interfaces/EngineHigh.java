f5ace27569fd337493b665dfdd0c87c7e90b9789
yisy0703 <yisy0703@naver.com>
2022-12-12 16:49:43 +0900

    java_ch12 pattern

---
+++ source/01_Java/ch13_pattern/src/strategy2/interfaces/EngineHigh.java
@@ -0,0 +1,7 @@
+package strategy2.interfaces;
+public class EngineHigh implements EngineImpl {
+	@Override
+	public void engine() {
+		System.out.println("최고급 엔진입니다");
+	}
+}
---
+++ source/01_Java/ch13_pattern/src/strategy2/interfaces/EngineLow.java
@@ -0,0 +1,7 @@
+package strategy2.interfaces;
+public class EngineLow implements EngineImpl {
+	@Override
+	public void engine() {
+		System.out.println("저급 엔진입니다");
+	}
+}
