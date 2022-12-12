e90a51e7f1b9c99d23bfe4dc6e7059bfc63f02e3
yisy0703 <yisy0703@naver.com>
2022-12-12 17:10:32 +0900

    java_ch12 pattern

--- source/01_Java/ch13_pattern/src/strategy2/modularization/TestMain.java
+++ source/01_Java/ch13_pattern/src/strategy2/modularization/TestMain.java
@@ -1,5 +1,8 @@
 package strategy2.modularization;
 
+import strategy2.interfaces.FuelHybrid;
+import strategy2.interfaces.Km20;
+
 public class TestMain {
 	public static void main(String[] args) {
 		Genesis genesis = new Genesis();
@@ -13,5 +16,31 @@ public class TestMain {
 			car.isKmPerLitter();
 			car.isFuel();
 		}
+		System.out.println("Sonata의 FuleHybrid로 교체하고 연비를 20으로 업그레이드");
+		sonata.setFuel(new FuelHybrid());
+		sonata.setKm(new Km20());
+		for(Car car : cars) {
+			car.shape();
+			car.drive();
+			car.isEngine();
+			car.isKmPerLitter();
+			car.isFuel();
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
