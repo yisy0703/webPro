637d5c5edf2f825e7d9fb02308269bc23e4a7a56
yisy0703 <yisy0703@naver.com>
2022-12-13 12:21:50 +0900

    java_ch14.api String Date

--- source/01_Java/ch14_api/src/com/lec/ex1_string/Ex02_StringApiMethod.java
+++ source/01_Java/ch14_api/src/com/lec/ex1_string/Ex02_StringApiMethod.java
@@ -3,7 +3,7 @@ public class Ex02_StringApiMethod {
 	public static void main(String[] args) {
 		String str1 = "abcXabc";
 		String str2 = new String("abcXabc");
-		String str3 = "  ja  va   ";
+		String str3 = "  ja  va2";
 		System.out.println("1." + str1.concat(str2)); // 1.abcXabcabcXabc
 		System.out.println("2." + str1.substring(3)); // 3번째부터 끝까지 : 2.Xabc
 		System.out.println("3." + str1.substring(3,5));//3번째부터5번째 앞까지 : 3.Xa
@@ -13,10 +13,18 @@ public class Ex02_StringApiMethod {
 		System.out.println("7." + str1.charAt(3)); // 3번째 문자 : 7.X
 		System.out.println("8." + str1.indexOf('b')); //첫번째 b가 나오는 인덱스 : 8.1
 		System.out.println("9." + str1.lastIndexOf("b"));//마지막 b가 나오는 인덱스 : 9.5
-		String str4 = "abcXabcXabcXabc";
-		System.out.println("10." + str1.indexOf('b', 3));//3번째인덱스부터 나오는 첫 b :10.5
-		System.out.println("11." + str1.indexOf("cXa")); // 처음 나오는 cXa의 인덱스 : 11.2
-		
+		String str4 = "abcXabcXabcXabc@";
+		System.out.println("10." + str4.indexOf('b', 3));//3번째인덱스부터 나오는 첫 b :10.5
+		System.out.println("11." + str4.indexOf("cXa")); // 처음 나오는 cXa의 인덱스 : 11.2
+		System.out.println("12." + str4.indexOf("cXa", 9));//9번째부터 나오는 첫 cXa : 12.10
+		System.out.println("13." + str4.indexOf("@")); // 없으면 -1 : 13.-1
+		System.out.println("14." + str1.equals(str2));//같은스트링은 true 14.true
+		System.out.println("15." + str1.equalsIgnoreCase("abcxabc"));//대소문자구별없이비교
+		System.out.println("16." + str3.trim()); // 앞뒤space제거 : 16.ja va2
+		System.out.println("17." + str4.replace("abc", "바꿔"));//17.바꿔X바꿔X바꿔X바꿔@
+		System.out.println("str1=" + str1);		
+		System.out.println("str3=" + str3);	
+		System.out.println("str4=" + str4);	
 	}
 }
 
---
+++ source/01_Java/ch14_api/src/com/lec/ex1_string/Ex03_regex.java
@@ -0,0 +1,13 @@
+package com.lec.ex1_string;
+public class Ex03_regex {
+	public static void main(String[] args) {
+		/* 정규표현식(regex)
+		  1. 참조 (https://goo.gl/HLntbd 위키백과)
+		  2. 간략문법
+		  
+		  3. 정규표현식 연습장
+		  
+		  4. 소스 구현
+		 */
+	}
+}
