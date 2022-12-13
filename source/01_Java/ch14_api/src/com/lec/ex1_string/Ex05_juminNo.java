e94344ff142513f40752c9ff1b092adf67cb7c9a
yisy0703 <yisy0703@naver.com>
2022-12-13 15:04:58 +0900

    java_ch14.api String Date

---
+++ source/01_Java/ch14_api/src/com/lec/ex1_string/Ex05_juminNo.java
@@ -0,0 +1,28 @@
+package com.lec.ex1_string;
+import java.util.Scanner;
+// 주민번호를 입력받아 성별 출력
+public class Ex05_juminNo {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("주민번호 ?");
+		String juminNo = scanner.next(); //"920225-2012121" 
+		// 방법1 char로 추출
+		char gender = juminNo.charAt(7); // '2'
+		if(gender=='1' || gender=='3') {
+			System.out.println("남성이시군요");
+		}else if(gender=='2' || gender=='4') {
+			System.out.println("여성이시군요");
+		}else {
+			System.out.println("유효하지 않은 주민번호입니다");
+		}
+		// 방법2 String으로 추출
+		String genderStr = juminNo.substring(7,8); // "2"
+		if(genderStr.equals("1") || genderStr.equals("3")) {
+			System.out.println("남성이시군요");
+		}else if(genderStr.equals("2") || genderStr.equals("4")) {
+			System.out.println("여성이시군요");
+		}else {
+			System.out.println("유효하지 않은 주민번호입니다");
+		}
+	}
+}
