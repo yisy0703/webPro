0deb66433e54e12befde5ca1b135f0dc375c4d3f
yisy0703 <yisy0703@naver.com>
2022-12-13 15:11:52 +0900

    java_ch14.api String Date

--- source/01_Java/ch14_api/src/com/lec/ex1_string/Ex05_juminNo.java
+++ source/01_Java/ch14_api/src/com/lec/ex1_string/Ex05_juminNo.java
@@ -24,9 +24,21 @@ public class Ex05_juminNo {
 		}else {
 			System.out.println("유효하지 않은 주민번호입니다");
 		}
-		// 방법3.genderStr을 정수로 전환
+		// 방법3. genderStr을 정수로 전환
+		int genderInt = Integer.parseInt(genderStr); // 정수스트링을 정수로 ("2"을 2로)
+		if(genderInt==1 || genderInt ==3) {
+			System.out.println("남성이시군요");
+		}else if(genderInt == 2 || genderInt==4) {
+			System.out.println("여성이시군요");
+		}else {
+			System.out.println("유효하지 않은 주민번호입니다");
+		}
 	}
 }
+
+
+
+
 
 
 
