ac20edc608c9a68aa9b1b8abbdd8f41d0df852cd
yisy0703 <yisy0703@naver.com>
2022-12-13 17:55:08 +0900

    java_ch14.api String Date

--- source/01_Java/ch14_api/src/com/lec/ex1_string/Ex06_telPrint.java
+++ source/01_Java/ch14_api/src/com/lec/ex1_string/Ex06_telPrint.java
@@ -1,8 +1,8 @@
 package com.lec.ex1_string;
 import java.util.Scanner;
-//  전화번호 입력받아 요리조리 별거 다 출력.
+//  전화번호 입력받아 전화번호, 짝수번째문자, 꺼꾸로, 국번, 뒷자리 출력
 public class Ex06_telPrint {
 	public static void main(String[] args) {
-				
-	}
-}
+		
+	}//main
+}//class
