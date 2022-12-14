e818cfe50698968c73c473dc933bc6bebc0856da
yisy0703 <yisy0703@naver.com>
2022-12-14 18:04:11 +0900

    java_ch14.api Object

---
+++ source/01_Java/ch14_api/src/com/lec/ex3_math/Ex06_lotto.java
@@ -0,0 +1,48 @@
+package com.lec.ex3_math;
+import java.util.Random;
+public class Ex06_lotto {
+	public static void main(String[] args) {
+		int[] lotto = new int[6];
+		int i, j;
+		int temp;
+		Random random = new Random();
+		for(i=0 ; i<lotto.length ; i++) {
+			do { // 발생된 난수가 중복되었는지 체크
+				temp = random.nextInt(45)+1;
+				for(j=0 ; j<i ; j++) {
+					if(lotto[j] == temp) {
+						break;
+					} // if - temp랑 같은 번호가 있으면 for문을 빠져나감.
+				}//for
+			}while(i!=j);
+			lotto[i] = temp;
+		}
+		// 발생된 로또 번호 출력
+		for(int l : lotto) {
+			System.out.print(l + "\t");
+		}
+		System.out.println("\n정렬 후");
+		// lotto 배열 값을 sort : 15 16 35 26 17 33
+		for(i=0; i<lotto.length-1 ; i++) {
+			for(j=i+1 ; j<lotto.length ; j++) {
+				if(lotto[i] > lotto[j]) {
+					temp = lotto[i];
+					lotto[i] = lotto[j];
+					lotto[j] = temp;
+				}
+			}
+		}
+		for(int l : lotto) {
+			System.out.print(l + "\t");
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+
---
+++ source/01_Java/ch14_api/src/com/lec/ex4_object/Person.java
@@ -0,0 +1,17 @@
+package com.lec.ex4_object;
+public class Person {
+	private long juminNo; //9812122512541
+	public Person() {}
+	public Person(long juminNo) {
+		this.juminNo = juminNo;
+	}
+	@Override
+	public String toString() {
+		return "주민번호는 " + juminNo;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// this의 주민번호와 obj의 주민번호가 같은지 다른지 여부를 return
+		return (this == obj); // p1.equals(p2) : p1이 this. p2가 obj
+	}
+}
