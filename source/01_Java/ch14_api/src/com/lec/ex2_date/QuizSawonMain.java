cc80588c9f8dd6459ee28c310f6007d5db8e9105
yisy0703 <yisy0703@naver.com>
2022-12-14 14:40:23 +0900

    java_ch14.api Object

---
+++ source/01_Java/ch14_api/src/com/lec/ex2_date/QuizSawonMain.java
@@ -0,0 +1,15 @@
+package com.lec.ex2_date;
+
+public class QuizSawonMain {
+	public static void main(String[] args) {
+		Sawon[] employee = {new Sawon("a01", "홍길동", Sawon.COMPUTER),
+							new Sawon("a02","신길동", Sawon.DESIGN, 2022,12,15),
+							new Sawon("b01","마길동", Sawon.ACCOUNTING, 2022, 12, 12)};
+		for(int i=0 ; i<employee.length ; i++) {
+			System.out.println(employee[i]);
+		}
+		for(Sawon emp : employee) {
+			System.out.println(emp);
+		}//for
+	}//main
+}
--- source/01_Java/ch14_api/src/com/lec/ex2_date/Sawon.java
+++ source/01_Java/ch14_api/src/com/lec/ex2_date/Sawon.java
@@ -1,6 +1,38 @@
 package com.lec.ex2_date;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
 public class Sawon {
-	private String no;
-	private String name;
-	
+	public static final String COMPUTER 	  = "COMPUTER";
+	public static final String PLANNING       = "PLANNING";
+	public static final String DESIGN         = "DESIGN";
+	public static final String ACCOUNTING     = "ACCOUNTING";
+	public static final String HUMANRESOURCES = "HUMANRESOURCES";
+	private String no;       // 사번
+	private String name;     // 이름
+	private String dept;     // 부서
+	private Date hiredate;   // 입사일
+	public Sawon(String no, String name, String dept) {
+		this.no = no;
+		this.name = name;
+		this.dept = dept;
+		hiredate = new Date();
+	}
+	public Sawon(String no, String name, String dept, int y, int m, int d) {
+		this.no   = no;
+		this.name = name;
+		this.dept = dept;
+		// 특정한 y년 m월 d일로 초기화
+		// hiredate = new Date(y-1900, m-1, d);
+		hiredate = new Date(new GregorianCalendar(y, m-1, d).getTimeInMillis()); 
+	}
+	@Override
+	public String toString() {  // [사번]a01  [이름]홍길동    [부서]COMPUTER   [입사일]2022년3월31일(목)
+		SimpleDateFormat sdf = new SimpleDateFormat("yy년M월d일(E)");
+		if(dept.length()>8) {
+			return "[사번]=" + no + "\t[이름]" + name + "\t[부서]" + dept + "\t[입사일]" + sdf.format(hiredate);
+		}else {
+			return "[사번]=" + no + "\t[이름]" + name + "\t[부서]" + dept + "\t\t[입사일]" + sdf.format(hiredate);
+		}
+	}
 }
