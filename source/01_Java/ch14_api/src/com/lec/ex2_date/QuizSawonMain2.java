61d5acc03baf31affb19da3cde953655df9bc759
yisy0703 <yisy0703@naver.com>
2022-12-14 16:32:23 +0900

    java_ch14.api Object

--- source/01_Java/ch14_api/src/com/lec/ex2_date/QuizSawonMain2.java
+++ source/01_Java/ch14_api/src/com/lec/ex2_date/QuizSawonMain2.java
@@ -2,13 +2,13 @@ package com.lec.ex2_date;
 
 public class QuizSawonMain2 {
 	public static void main(String[] args) {
-		Sawon[] employee = {new Sawon("a01", "홍길동", "COMXXX"),
-							new Sawon("a02","신길동", Sawon.DESIGN, 2022,12,15),
-							new Sawon("b01","마길동", Sawon.HUMANRESOURCES, 2022, 12, 12)};
+		Sawon2[] employee = {new Sawon2("a01", "홍길동", Dept.COMPUTER),
+							new Sawon2("a02","신길동", Dept.ACCOUNTING, 2022,12,15),
+							new Sawon2("b01","마길동", Dept.HUMANRESOURCES, 2022, 12, 12)};
 		for(int i=0 ; i<employee.length ; i++) {
 			System.out.println(employee[i]);
 		}
-		for(Sawon emp : employee) {
+		for(Sawon2 emp : employee) {
 			System.out.println(emp);
 		}//for
 	}//main
