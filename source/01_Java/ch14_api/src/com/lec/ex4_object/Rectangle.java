d94e940d88208d0ab55c5dbd3105efa19aa7417c
yisy0703 <yisy0703@naver.com>
2022-12-15 12:19:35 +0900

    java_ch14.api timer

--- source/01_Java/ch14_api/src/com/lec/ex4_object/Rectangle.java
+++ source/01_Java/ch14_api/src/com/lec/ex4_object/Rectangle.java
@@ -32,6 +32,24 @@ public class Rectangle {
 		}
 		return false;
 	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
 }
 
 
