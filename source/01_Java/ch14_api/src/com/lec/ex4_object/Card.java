7938e513f565a0f3cc6631523b744ed87bff1022
yisy0703 <yisy0703@naver.com>
2022-12-15 12:08:41 +0900

    java_ch14.api timer

--- source/01_Java/ch14_api/src/com/lec/ex4_object/Card.java
+++ source/01_Java/ch14_api/src/com/lec/ex4_object/Card.java
@@ -15,7 +15,13 @@ public class Card {
 	public boolean equals(Object obj) {
 		// card.equals(comCard) => card가 this. comCard가 obj
 		// obj가 null이 아니고 obj가 Card타입 객체인지 확인후, kind와 num비교 결과 return 
-		
+		if(obj != null && getClass()==obj.getClass()) { //if(obj!=null && obj instanceof Card) {	
+			// kind와 num 비교 결과 return 
+			Card other = (Card)obj;
+			boolean kindChk = kind == other.kind; // kind.equals(other.kind)
+			boolean numChk  = num  == other.num;
+			return kindChk && numChk;
+		}
 		return super.equals(obj);
 	}
 }
---
+++ source/01_Java/ch14_api/src/com/lec/ex4_object/Rectangle.java
@@ -0,0 +1,51 @@
+package com.lec.ex4_object;
+// new Rectangle(10,5,"빨강") new Rectangle(10,5) new Rectangle() new Rectanlge("노랑")
+public class Rectangle {
+	private int width;
+	private int height;
+	private String color;
+	public Rectangle() { color = "검정";	}
+	public Rectangle(String color) {this.color = color;}
+	public Rectangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+		color = "검정";
+	}
+	public Rectangle(int width, int height, String color) {
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
+	@Override
+	public String toString() { // 객체의 정보를 스트링으로 return
+		//return "가로 " + width + "cm, 세로 " + height +"cm의 " + color +"색 사각형";
+		return String.format("가로 %dcm 세로 %dcm의 %s색 사각형", width, height, color);
+	}
+	@Override
+	public boolean equals(Object obj) { // r1.equals(r2) => r1이 this, r2가 obj
+		if(obj!=null && getClass() == obj.getClass()) {
+			Rectangle other = (Rectangle)obj;
+			boolean widthChk = width==other.width;
+			boolean heightChk = height==other.height;
+			boolean colorChk = color.equals(other.color);
+			return widthChk && heightChk && colorChk;
+		}
+		return false;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
