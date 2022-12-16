6d20b2f2719fc6eb99a412121a113717784b067a
yisy0703 <yisy0703@naver.com>
2022-12-16 12:21:10 +0900

    java_ch16.collection

--- source/01_Java/ch15_exception/src/com/lec/ex2_throws/TestMain.java
+++ source/01_Java/ch15_exception/src/com/lec/ex2_throws/TestMain.java
@@ -2,6 +2,11 @@ package com.lec.ex2_throws;
 
 public class TestMain {
 	public static void main(String[] args) {
-		new ThrowsEx();
+		try {
+			new ThrowsEx();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
