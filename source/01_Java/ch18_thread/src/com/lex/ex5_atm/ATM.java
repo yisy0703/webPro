303221a657da170f01e34f8be7b9c9a83bca464a
yisy0703 <yisy0703@naver.com>
2022-12-21 16:08:01 +0900

    java_ch18.thread

--- source/01_Java/ch18_thread/src/com/lex/ex5_atm/ATM.java
+++ source/01_Java/ch18_thread/src/com/lex/ex5_atm/ATM.java
@@ -4,13 +4,13 @@ public class ATM {
 	public ATM(int balance) {
 		this.balance = balance;
 	}
-	public void deposit(int amount, String who) {
+	public synchronized void deposit(int amount, String who) {
 		System.out.println(who+"가 입금 시작 ~~~~~~~");
 		balance += amount;
 		System.out.println(who + "가 " + amount +"원 입금하여 잔액 : " + balance);
 		System.out.println(who+"가 입금 끝 ~~~~~~~~");
 	}
-	public void withdraw(int amount, String who) {
+	public synchronized void withdraw(int amount, String who) {
 		System.out.println(who+"가 출금 시작 ★★★★★★★★★★");
 		balance -= amount;
 		System.out.println(who+"가 " + amount +"원 출금하여 잔액 : " + balance);
