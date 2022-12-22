8862c03f014b85ce8970f93c2bb01768091d4f06
yisy0703 <yisy0703@naver.com>
2022-12-22 12:03:33 +0900

    dbms

--- source/02_DBMS/221222.2_SELECT.sql
+++ source/02_DBMS/221222.2_SELECT.sql
@@ -76,8 +76,38 @@ SELECT ENAME, NVL(TO_CHAR(MGR),'CEO') MGR FROM EMP;
 -- 6. 연결연산자(||) : 필드나 문자를 연결
 SELECT ENAME || '은' || JOB EMPLOYEE FROM EMP;
     -- ex. 모든 사원에 대하여 'SMITH : ANNUAL SALARY = XXXX' 포맷으로 출력 (연봉=SAL*12+COMM)
-SELECT ENAME || ' : ANNUAL SALARY = ' || (SAL*12+NVL(COMM,0)) MGESSAGE FROM EMP;
+SELECT ENAME || ' : ANNUAL SALARY = ' || (SAL*12+NVL(COMM,0)) MESSAGE FROM EMP;
 
+-- 7. 중복제거(DISTINCT)
+SELECT DISTINCT JOB FROM EMP;
+SELECT DISTINCT DEPTNO FROM EMP;
+
+-- ★ 연습문제 꼭 풀기
+--1. emp 테이블의 구조 출력
+
+--2. emp 테이블의 모든 내용을 출력 
+
+--3. 현 scott 계정에서 사용가능한 테이블 출력
+
+--4. emp 테이블에서 사번, 이름, 급여, 업무, 입사일 출력
+
+--5. emp 테이블에서 급여가 2000미만인 사람의 사번, 이름, 급여 출력
+
+--6. 입사일이 81/02이후에 입사한 사람의 사번, 이름, 업무, 입사일 출력
+
+--7. 업무가 SALESMAN인 사람들 모든 자료 출력
+
+--8. 업무가 CLERK이 아닌 사람들 모든 자료 출력
+
+--9. 급여가 1500이상이고 3000이하인 사람의 사번, 이름, 급여 출력
+
+--10. 부서코드가 10번이거나 30인 사람의 사번, 이름, 업무, 부서코드 출력
+
+--11. 업무가 SALESMAN이거나 급여가 3000이상인 사람의 사번, 이름, 업무, 부서코드 출력
+
+--12. 급여가 2500이상이고 업무가 MANAGER인 사람의 사번, 이름, 업무, 급여 출력
+
+--13.“ename은 XXX 업무이고 연봉은 XX다” 스타일로 모두 출력(연봉은 SAL*12+COMM)
 
 
 
