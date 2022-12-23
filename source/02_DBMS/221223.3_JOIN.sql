4244f7aa74bb41904a48af6e72092f6d82a3fb06
yisy0703 <yisy0703@naver.com>
2022-12-23 11:00:32 +0900

    dbms_join

--- source/02_DBMS/221223.3_JOIN.sql
+++ source/02_DBMS/221223.3_JOIN.sql
@@ -64,7 +64,24 @@ SELECT * FROM SALGRADE; -- 급여 등급 정보
 SELECT * FROM EMP, SALGRADE WHERE ENAME='SCOTT'; -- CROSS JOIN
 SELECT * FROM EMP, SALGRADE WHERE SAL >= LOSAL AND SAL <= HISAL AND ENAME='SCOTT';-- NONEQUI
 SELECT * FROM EMP, SALGRADE WHERE SAL BETWEEN LOSAL AND HISAL AND ENAME='SCOTT'; -- NONEQUI
+    --ex. 모든사원의 사번, 이름, 직책, 상사사번, 급여, 급여등급(1등급, 2등급, ..)
+SELECT EMPNO, ENAME, JOB, MGR, SAL, GRADE||'등급' GRADE
+    FROM EMP, SALGRADE
+    WHERE SAL BETWEEN LOSAL AND HISAL;
 
+    --탄탄ex1 Comm이 null이 아닌 사원의 이름, 급여, 등급, 부서번호, 부서이름, 근무지를 출력하시오.
+
+    --탄탄ex2 이름, 급여, 입사일, 급여등급
+
+    --탄탄ex3 이름, 급여, 입사일, 급여등급, 부서명, 근무지
+
+    --탄탄ex4 이름, 급여, 등급, 부서코드, 근무지. 단 comm 이 null아닌 경우
+
+    --탄탄ex5 이름, 급여, 급여등급, 연봉, 부서명, 부서별 출력, 부서가 같으면 연봉순. 연봉=(sal+comm)*12 comm이 null이면 0
+
+    --탄탄ex6 이름, 업무, 급여, 등급, 부서코드, 부서명 출력. 급여가 1000~3000사이. 정렬조건 : 부서별, 부서같으면 업무별, 업무같으면 급여 큰순
+
+    --탄탄ex7 이름, 급여, 등급, 입사일, 근무지. 81년에 입사한 사람. 등급 큰순
 
 
 
