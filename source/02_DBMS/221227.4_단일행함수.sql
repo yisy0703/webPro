ee1880b87b94bb3fec1d7582f13384a33b92bfa2
yisy0703 <yisy0703@naver.com>
2022-12-23 18:06:57 +0900

    dbms_join

--- source/02_DBMS/221227.4_단일행함수.sql
+++ source/02_DBMS/221227.4_단일행함수.sql
@@ -30,6 +30,30 @@ SELECT MOD(9,2) FROM DUAL; -- 나머지 연산자 (9%2)
     -- ex. 홀수년도에 입사한 사원의 모든 정보 출력
 SELECT * FROM EMP WHERE MOD(TO_CHAR(HIREDATE, 'RR'), 2) = 1;
 
+-- 2. 문자관련함수 
+-- ① 대소문자 관련
+SELECT INITCAP('WELCOME TO ORACLE') FROM DUAL; -- 첫문자만 대문로
+SELECT INITCAP('welcome to oracle') FROM DUAL;
+SELECT UPPER('ABCabc') FROM DUAL; -- 대문자로
+SELECT LOWER('ABCabc') FROM DUAL; -- 소문자로
+    -- ex. 이름이 SCOTT 인 직원의 모든 필드
+SELECT * FROM EMP WHERE UPPER(ENAME)='SCOTT';
+SELECT * FROM EMP WHERE INITCAP(ENAME)='Scott';
+    -- ex. job이 MANAGER 인 직원의 모든 필드
+SELECT * FROM EMP WHERE UPPER(JOB) = 'MANAGER';
+SELECT * FROM EMP WHERE INITCAP(JOB) = 'Manager';
+SELECT * FROM EMP WHERE LOWER(JOB) = 'manager';
+
+-- ② 문자연결(concat함수, ||연산자)
+SELECT 'AB'||'CD'||'EF'||'GH' FROM DUAL;
+SELECT CONCAT(CONCAT('AB','CD'), CONCAT('EF','GH')) FROM DUAL;
+    -- ex. SMITH는 manager다
+SELECT CONCAT(CONCAT(ENAME, '는 '), CONCAT(JOB, '다')) FROM EMP;
+SELECT ENAME || '는 ' || JOB || '다' FROM EMP;
+
+
+
+
 
 
 
