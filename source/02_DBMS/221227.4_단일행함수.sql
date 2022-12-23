2f4f6627524eaffb7e8213e2f41b145b42f24b6c
yisy0703 <yisy0703@naver.com>
2022-12-23 17:50:54 +0900

    dbms_join

--- source/02_DBMS/221227.4_단일행함수.sql
+++ source/02_DBMS/221227.4_단일행함수.sql
@@ -1,2 +1,35 @@
 -- [ IV ] 단일행함수
 -- 함수 = 단일행함수 + 그룹함수(집계함수)
+SELECT HIREDATE, TO_CHAR(HIREDATE, 'YYYY-MM-DD') FROM EMP; -- 단일행 단일행 적용
+SELECT ENAME, INITCAP(ENAME) FROM EMP; -- 단일행함수(INPUT 1행 -> OUTPUT 1행)
+SELECT SUM(SAL), AVG(SAL) FROM EMP; -- 그룹함수(INPUT n행 -> OUTPUT 1행)
+SELECT DEPTNO, AVG(SAL) FROM EMP GROUP BY DEPTNO;-- 그룹함수(INPUT n행 -> OUTPUT 1행)
+
+-- ★ 단일행함수의 종류 : 숫자관련함수, 문자관련함수, 날짜관련함수, 형변환함수, NVL(), ETC...
+-- 1. 숫자관련함수 
+    -- DUAL테이블 : 오라클에서 제공한 1행1열짜리 DUMMY TABLE
+SELECT * FROM DUAL;
+DESC DUAL;
+SELECT * FROM DUMMY;
+DESC DUMMY;
+SELECT ABS(-1) FROM DUMMY; -- 절대값
+SELECT FLOOR(34.5678) FROM DUAL; -- 소수점에서 내림(버림)
+SELECT FLOOR(34.5678*100)/100 FROM DUAL; -- 소수점 두번째 자리에서 내림(버림)
+SELECT TRUNC(34.5678, 2) FROM DUAL; -- 소수점 두번째 자리에서 내림(버림)
+SELECT TRUNC(34.5678) FROM DUAL;    -- 소수점에서 내림(버림)
+SELECT TRUNC(34.5678, -1) FROM DUAL; -- 일의 자리에서 내림(버림)
+    -- ex. EMP테이블에서 이름, 급여(십의 자리에서 내림)
+SELECT ENAME, SAL, TRUNC(SAL, -2) FROM EMP;
+SELECT CEIL(34.5678) FROM DUAL; -- 소수점에서 올림
+
+SELECT ROUND(34.5678) FROM DUAL; -- 소수점에서 반올림
+SELECT ROUND(34.5678, 2) FROM DUAL; -- 소수점 두번째 자리까지 나오도록 반올림
+SELECT ROUND(34.5678, -1) FROM DUAL;   -- 일의 자리에서 반올림
+
+SELECT MOD(9,2) FROM DUAL; -- 나머지 연산자 (9%2)
+    -- ex. 홀수년도에 입사한 사원의 모든 정보 출력
+SELECT * FROM EMP WHERE MOD(TO_CHAR(HIREDATE, 'RR'), 2) = 1;
+
+
+
+
