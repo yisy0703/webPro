264bd011e7ecc19d6648d0fc603a2225d45737a2
yisy0703 <yisy0703@naver.com>
2022-12-27 11:07:53 +0900

    dbms_groupFunction

--- source/02_DBMS/221227.4_단일행함수.sql
+++ source/02_DBMS/221227.4_단일행함수.sql
@@ -104,7 +104,7 @@ DESC EMP; -- 이름은 10문자까지 SAL은 5자리까지 입력 가능
 SELECT RPAD(ENAME, 11, '-'), LPAD(SAL, 6, '*') FROM EMP;
     -- ex. 사번, 이름(7369 S**** / 7654 M***** / 7521 W*** ..) - SUBSTR, LENGTH, RPAD
     
-    -- ex. 사번, 이름, 직책(9자리확보), 입사일 
+    -- ex. 사번, 이름, 직책(9자리확보), 입사일 -- LPAD, SUBSTR, LENGTH, ||, CONCAT, ...
       -- (7369 SMITH ____****K 80/12/**)
       -- (7499 ALLEN __******N 81/02/**)
       -- (7566 JONES ___*****R 81/02/**)
