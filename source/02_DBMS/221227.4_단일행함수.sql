c6db7ebb4d27105b8176cf5ac5a2f5e7cf08c784
yisy0703 <yisy0703@naver.com>
2022-12-27 15:52:08 +0900

    dbms_groupFunction

--- source/02_DBMS/221227.4_단일행함수.sql
+++ source/02_DBMS/221227.4_단일행함수.sql
@@ -252,7 +252,49 @@ SELECT SYSDATE, EXTRACT(MONTH FROM SYSDATE) FROM DUAL; -- 월만 숫자로 추출
 SELECT SYSDATE, TO_CHAR(SYSDATE, 'MM') FROM DUAL; -- 월2주리를 문자로 추출함
 SELECT SYSDATE, EXTRACT(DAY FROM SYSDATE) FROM DUAL; -- 일만 숫자로 추출
 SELECT SYSDATE, TO_CHAR(SYSDATE, 'DD') FROM DUAL;    -- 일 두자리를 문자로 추출
--- (2)
+-- (2) 레벨별 출력
+SELECT * FROM EMP;
+SELECT LPAD('ㄴ', LEVEL*2) || ENAME FROM EMP
+    START WITH MGR IS NULL
+    CONNECT BY PRIOR EMPNO=MGR;
+
+-- ★ <총 연습문제> ★
+-- 1. 현재 날짜를 출력하고 TITLE에 “Current Date”로 출력하는 SELECT 문장을 기술하시오.
+
+-- 2. EMP 테이블에서 현재 급여에 15%가 증가된 급여를 계산하여,
+-- 사원번호,이름,업무,급여,증가된 급여(New Salary),증가액(Increase)를 출력하는 SELECT 문장
+
+--3. 이름, 입사일, 입사일로부터 6개월 후 돌아오는 월요일 구하여 출력하는 SELECT 문장을 기술하시오.
+
+--4. 이름, 입사일, 입사일로부터 현재까지의 개월수, 급여, 입사일부터 현재까지의 받은 급여의 총계를 출력
+
+--5. 모든 사원의 이름과 급여(15자리로 출력 좌측의 빈 곳은 “*”로 대치)를 출력
+
+--6. 모든 사원의 정보를 이름,업무,입사일,입사한 요일을 출력하는 SELECT 문장을 기술하시오.
+
+--7. 이름의 길이가 6자 이상인 사원의 정보를 이름,이름의 글자수,업무를 출력
+
+--8. 모든 사원의 정보를 이름, 업무, 급여, 보너스, 급여+보너스를 출력
+
+-- 9.사원 테이블의 사원명에서 2번째 문자부터 3개의 문자를 추출하시오. 
+
+--10. 사원 테이블에서 입사일이 12월인 사원의 사번, 사원명, 입사일을 검색하시오. 
+--  시스템에 따라 DATEFORMAT 다를 수 있으므로 아래의 방법도 알아보자
+
+--11. 다음과 같은 결과를 검색할 수 있는 SQL 문장을 작성하시오
+--EMPNO		ENAME		급여
+--7369		       SMITH		*******800
+--7499		       ALLEN		******1600
+--7521		       WARD		******1250
+--……. 
+
+-- 12. 다음과 같은 결과를 검색할 수 있는 SQL 문장을 작성하시오
+-- EMPNO	 ENAME 	입사일
+-- 7369	  SMITH		1980-12-17
+-- ….
+
+--13. 사원 테이블에서 부서 번호가 20인 사원의 사번, 이름, 직무, 급여를 출력하시오.
+    --(급여는 앞에 $를 삽입하고3자리마다 ,를 출력한다)
 
 
 
