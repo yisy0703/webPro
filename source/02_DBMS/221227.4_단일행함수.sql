05174561781069bdc7829b88d65e0971f1343667
yisy0703 <yisy0703@naver.com>
2022-12-27 16:56:43 +0900

    dbms_groupFunction

--- source/02_DBMS/221227.4_단일행함수.sql
+++ source/02_DBMS/221227.4_단일행함수.sql
@@ -260,42 +260,53 @@ SELECT LPAD('ㄴ', LEVEL*2) || ENAME FROM EMP
 
 -- ★ <총 연습문제> ★
 -- 1. 현재 날짜를 출력하고 TITLE에 “Current Date”로 출력하는 SELECT 문장을 기술하시오.
-
+SELECT SYSDATE "Current Date" FROM DUAL;
 -- 2. EMP 테이블에서 현재 급여에 15%가 증가된 급여를 계산하여,
 -- 사원번호,이름,업무,급여,증가된 급여(New Salary),증가액(Increase)를 출력하는 SELECT 문장
-
+SELECT EMPNO, ENAME, JOB, SAL, SAL*1.15 "New Salary", SAL*0.15 Increase 
+    FROM EMP;
 --3. 이름, 입사일, 입사일로부터 6개월 후 돌아오는 월요일 구하여 출력하는 SELECT 문장을 기술하시오.
-
---4. 이름, 입사일, 입사일로부터 현재까지의 개월수, 급여, 입사일부터 현재까지의 받은 급여의 총계를 출력
-
+SELECT ENAME, HIREDATE, 
+        NEXT_DAY(ADD_MONTHS(HIREDATE, 6), '월')
+    FROM EMP;
+--4. 이름, 입사일, 입사일로부터 현재까지의 개월수, 급여, 입사일부터 현재까지의 받은 급여의 총계를 
+    -- 출력
+SELECT ENAME, HIREDATE,
+    TRUNC(MONTHS_BETWEEN(SYSDATE, HIREDATE)), SAL, 
+    TRUNC(MONTHS_BETWEEN(SYSDATE, HIREDATE))*SAL*12 FROM EMP;
+SELECT ENAME, HIREDATE,
+        TRUNC(MONTHS_BETWEEN(SYSDATE, HIREDATE)), SAL, 
+        TO_CHAR(TRUNC(MONTHS_BETWEEN(SYSDATE, HIREDATE))*SAL*12, '$99,999,999')
+    FROM EMP;    
 --5. 모든 사원의 이름과 급여(15자리로 출력 좌측의 빈 곳은 “*”로 대치)를 출력
-
+SELECT ENAME, LPAD(SAL, 15, '*') FROM EMP;
 --6. 모든 사원의 정보를 이름,업무,입사일,입사한 요일을 출력하는 SELECT 문장을 기술하시오.
-
+SELECT ENAME, JOB, HIREDATE, TO_CHAR(HIREDATE, 'DY"요일"') from EMP;
+SELECT ENAME, JOB, HIREDATE, TO_CHAR(HIREDATE, 'DAY') from EMP;
 --7. 이름의 길이가 6자 이상인 사원의 정보를 이름,이름의 글자수,업무를 출력
-
+SELECT ENAME, LENGTH(ENAME), JOB FROM EMP WHERE LENGTH(ENAME)>= 6;
 --8. 모든 사원의 정보를 이름, 업무, 급여, 보너스, 급여+보너스를 출력
-
+SELECT ENAME, JOB, SAL, COMM, SAL+NVL(COMM, 0) FROM EMP;
 -- 9.사원 테이블의 사원명에서 2번째 문자부터 3개의 문자를 추출하시오. 
-
+SELECT SUBSTR(ENAME, 2, 4) FROM EMP;
 --10. 사원 테이블에서 입사일이 12월인 사원의 사번, 사원명, 입사일을 검색하시오. 
 --  시스템에 따라 DATEFORMAT 다를 수 있으므로 아래의 방법도 알아보자
-
+SELECT EMPNO, ENAME, HIREDATE FROM EMP WHERE EXTRACT(MONTH FROM HIREDATE)=12;
+SELECT EMPNO, ENAME, HIREDATE FROM EMP WHERE TO_CHAR(HIREDATE, 'MM') = '12';
 --11. 다음과 같은 결과를 검색할 수 있는 SQL 문장을 작성하시오
 --EMPNO		ENAME		급여
---7369		       SMITH		*******800
---7499		       ALLEN		******1600
---7521		       WARD		******1250
+--7369		SMITH		*******800
+--7499		ALLEN		******1600
+--7521		WARD		******1250
 --……. 
-
+SELECT EMPNO, ENAME, LPAD(SAL, 10, '*') 급여 FROM EMP;
 -- 12. 다음과 같은 결과를 검색할 수 있는 SQL 문장을 작성하시오
 -- EMPNO	 ENAME 	입사일
 -- 7369	  SMITH		1980-12-17
 -- ….
-
+SELECT EMPNO, ENAME, TO_CHAR(HIREDATE, 'YYYY-MM-DD') 입사일 FROM EMP;
 --13. 사원 테이블에서 부서 번호가 20인 사원의 사번, 이름, 직무, 급여를 출력하시오.
     --(급여는 앞에 $를 삽입하고3자리마다 ,를 출력한다)
-
-
-
+DESC EMP; -- SAL NUMBER 자리수 확인
+SELECT EMPNO, ENAME, JOB, TO_CHAR(SAL, '$99,999') FROM EMP WHERE DEPTNO=20;
 
