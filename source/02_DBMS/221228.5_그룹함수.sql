c76cf74d9a2585051b78cad7027183dc58c75c72
yisy0703 <yisy0703@naver.com>
2022-12-28 09:56:05 +0900

    dbms_groupFunction

--- source/02_DBMS/221228.5_그룹함수.sql
+++ source/02_DBMS/221228.5_그룹함수.sql
@@ -15,11 +15,15 @@ SELECT COUNT(*) FROM EMP; -- 테이블 행수
 SELECT ROUND(AVG(SAL),1), SUM(SAL), MIN(SAL), MAX(SAL), 
         ROUND(VARIANCE(SAL),1), ROUND(STDDEV(SAL),1)
     FROM EMP;
+-- 그룹함수 MIN, MAX, COUNT는 숫자형, 문자형, 날짜형 모두 가능
+
 SELECT AVG(SAL)-2*STDDEV(SAL), AVG(SAL)+2*STDDEV(SAL) FROM EMP; -- 95.44%의 SAL
 -- 탄탄ex1. 입사한지 가장 오래된 사원의 입사일과 가장 최근에 입사한 사원의 입사일을 출력. 
- 
+SELECT MIN(HIREDATE), MAX(HIREDATE) FROM EMP;
 -- 탄탄ex2. (결과) 80/12/17:14620일째 83/01/12:13864일째
-
+SELECT MIN(HIREDATE)||':'||TRUNC(SYSDATE-MIN(HIREDATE))||'일째' FIRST,
+        MAX(HIREDATE)||':'||TRUNC(SYSDATE-MAX(HIREDATE))||'일째' LAST
+    FROM EMP;
 
 -- 탄탄ex3.  (결과) 80년12월17일 최초입사:14,620일째 83년01월12일 최근입사 :13,864일째
 
