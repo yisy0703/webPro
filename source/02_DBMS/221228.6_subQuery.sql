5083aa88b1db77ff3cf1c310b18219527f8c76be
yisy0703 <yisy0703@naver.com>
2022-12-28 13:54:20 +0900

    dbms_groupFunction

--- source/02_DBMS/221228.6_subQuery.sql
+++ source/02_DBMS/221228.6_subQuery.sql
@@ -1,2 +1,24 @@
 -- [ VI ] Sub Query : 메인 QUERY(SQL문) 안에 QUERY(SQL문)이 내포
 
+-- ★ 1. 서브 쿼리 개념
+-- 서브쿼리가 필요한 ex. 급여를 제일 많이 받는 사람의 이름과 급여
+    SELECT ENAME, MAX(SAL) FROM EMP; -- 에러
+    SELECT ENAME, MAX(SAL) FROM EMP GROUP BY ENAME; -- 다 찍혀
+    SELECT MAX(SAL) FROM EMP; -- 서브쿼리
+    SELECT ENAME, SAL FROM EMP
+        WHERE SAL = (SELECT MAX(SAL) FROM EMP); -- 메인 쿼리
+-- 서브쿼리 종류(1) 단일행 서브쿼리(서브쿼리의 실행결과가 단일행) : = > >= < <= != 
+    -- ex. scott이 근무하는 부서이름 출력
+SELECT DEPTNO FROM EMP WHERE ENAME='SCOTT'; -- 서브쿼리
+SELECT DNAME FROM DEPT 
+    WHERE DEPTNO=(SELECT DEPTNO FROM EMP WHERE ENAME='SCOTT'); -- 메인 쿼리
+
+SELECT DNAME FROM EMP E, DEPT D WHERE E.DEPTNO=D.DEPTNO AND ENAME='SCOTT';-- JOIN이용
+
+
+
+
+
+
+
+
