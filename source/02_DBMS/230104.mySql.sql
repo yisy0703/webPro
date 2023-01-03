f79dbb93248bd4c665d70cd97a2959230e45569e
yisy0703 <yisy0703@naver.com>
2023-01-03 16:49:53 +0900

    DBMS_VIEW

--- source/02_DBMS/230104.mySql.sql
+++ source/02_DBMS/230104.mySql.sql
@@ -2,3 +2,21 @@
 -- DDL (제약조건, 시퀀스없음)
 -- DML (outer join, and;&&, or;||, 일부단일행함수)
 -- Java에서 쓸 데이터 넣고 연습해보기
+
+show databases; -- database들의 리스
+-- ★ ★ ★ DCL
+create user user01 identified by 'password'; -- 계정 생성
+grant all on *.* to user01; -- 권한 부여
+grant all privileges on *.* to user01;
+flush privileges; -- 
+revoke all on *.* from user01; -- 권한박탈
+drop user user01; -- 계정 삭제
+
+
+
+
+
+
+
+
+
