f3bde3fcece5412a7ba74839dcf6d71f59cc1b4a
yisy0703 <yisy0703@naver.com>
2023-01-04 15:07:21 +0900

    DBMS_MySQL

---
+++ source/01_Java/ch19_jdbc/src/com/lec/ex0Conn/ConnMySql.java
@@ -0,0 +1,9 @@
+package com.lec.ex0Conn;
+
+public class ConnMySql {
+	public static void main(String[] args) {
+		String driver = "com.mysql.cj.jdbc.Driver";
+		String url = "jdbc:mysql://127.0.0.1:3306/kimdb?serverTimezone=UTC";
+		
+	}
+}
