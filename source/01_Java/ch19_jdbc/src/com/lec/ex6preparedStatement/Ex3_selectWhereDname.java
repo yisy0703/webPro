c5214f2d3fc3765a3387c0a56713cf7e0fb87e98
yisy0703 <yisy0703@naver.com>
2023-01-05 14:44:44 +0900

    JDBC

--- source/01_Java/ch19_jdbc/src/com/lec/ex6preparedStatement/Ex3_selectWhereDname.java
+++ source/01_Java/ch19_jdbc/src/com/lec/ex6preparedStatement/Ex3_selectWhereDname.java
@@ -1,5 +1,32 @@
 package com.lec.ex6preparedStatement;
+import java.sql.*;
+import java.util.Scanner;
 // 부서명을 받아 해당 부서 사원을 출력(사번, 이름, 상사이름, 급여, 급여등급. 상사가 없는사람은 CEO)
 public class Ex3_selectWhereDname {
-
+	public static void main(String[] args) {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+		Scanner sc = new Scanner(System.in);
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		ResultSet         rs    = null;
+		String sql = "SELECT W.EMPNO, W.ENAME, NVL(M.ENAME, 'CEO') MANAGER, W.SAL, GRADE" + 
+				"  FROM EMP W, EMP M, DEPT D, SALGRADE" + 
+				"  WHERE W.MGR=M.EMPNO AND W.DEPTNO=D.DEPTNO " + 
+				"        AND W.SAL BETWEEN LOSAL AND HISAL" + 
+				"        AND DNAME=UPPER(?)";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
