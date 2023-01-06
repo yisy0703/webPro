c60fddce35c803c111f662adb340c55ca2cf4778
yisy0703 <yisy0703@naver.com>
2023-01-06 14:04:10 +0900

    JDBC

--- source/01_Java/lastEx/src/com/lec/ex2person_dtodao/PersonDao.java
+++ source/01_Java/lastEx/src/com/lec/ex2person_dtodao/PersonDao.java
@@ -1,4 +1,9 @@
 package com.lec.ex2person_dtodao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 public class PersonDao {
 	private String driver = "oracle.jdbc.driver.OracleDriver";
@@ -19,7 +24,38 @@ public class PersonDao {
 			System.out.println(e.getMessage());
 		} 
 	}
-	// 1번 기능 이름,직업명,국영수를 받아 DB에 insert하고 SUCCESS/FAIL을 return
+	// 1번 기능 dto(이름,직업명,국영수)를 받아 DB에 insert하고 SUCCESS/FAIL을 return
+	public int insertPerson(PersonDto dto) {
+		int result = FAIL;
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		String sql = "INSERT INTO PERSON" + 
+				" VALUES (PERSON_PNO_SQ.NEXTVAL, ?, (SELECT JNO FROM JOB WHERE JNAME=?)," + 
+				"            ?, ?, ?)";
+		try {
+			conn = DriverManager.getConnection(url, "scott", "tiger"); // 2
+			pstmt = conn.prepareStatement(sql); // 3
+			pstmt.setString(1, dto.getPname());
+			pstmt.setString(2, dto.getJname());
+			pstmt.setInt(3, dto.getKor());
+			pstmt.setInt(4, dto.getEng());
+			pstmt.setInt(5, dto.getMat());
+			result = pstmt.executeUpdate();
+			System.out.println(result==SUCCESS ? "성공":"실패");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return result;
+	}
+	// 2번 기능 직업명을 받아 DB에 select 한 결과를 ArrayList<PersonDto>로 return
+	public ArrayList<PersonDto> selectJname(String jname){
+		ArrayList<PersonDto> dtos = new ArrayList<PersonDto>();
+		// DB 결과를 dtos에 add한다
+		return dtos;
+	}
+	// 3번 기능 DB에 전체 select한 결과를 ArrayList<PersonDto>로 return
+	
+	// jname들을 Vector<String>로 return
 }
 
 
