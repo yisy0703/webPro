bbdaea7f13173e602d8f634bd21a0d3b82c06188
soyoung-Yi <spacenyi@naver.com>
2023-01-08 16:22:24 +0900

    JDBC

--- source/01_Java/lastEx/src/com/lec/ex2person_dtodao/PersonMng.java
+++ source/01_Java/lastEx/src/com/lec/ex2person_dtodao/PersonMng.java
@@ -1,11 +1,5 @@
 package com.lec.ex2person_dtodao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Scanner;
 
