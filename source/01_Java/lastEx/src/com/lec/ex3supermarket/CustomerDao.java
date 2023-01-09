d16d62f31f9d6f92d7071dee130b1740f9f4825c
yisy0703 <yisy0703@naver.com>
2023-01-09 12:08:41 +0900

    JDBC

--- source/01_Java/lastEx/src/com/lec/ex3supermarket/CustomerDao.java
+++ source/01_Java/lastEx/src/com/lec/ex3supermarket/CustomerDao.java
@@ -7,11 +7,16 @@ public class CustomerDao {
 	// dao는 싱글톤으로 구현/ private 생성자에는 driver로드
 	
 	// 1. 회원가입 : public int insertCustomer(String ctel, String cname)
+	
   // public int insertCustomer(CustomerDto dto)
+	
 	// 2. 폰뒤4자리(풀번호) 검색  : 
 	// public ArrayList<CustomerDto> ctelGetCustomers(String searchTel)
+	
 	// 3. 물품구입 : public int buy(int cid, int price)
+	
 	// 3번 물품구입 후 구매자 정보 출력 : public CustomerDto getCustomer(int cid)
+	
 	// 4번 전 levelName들 정보 추출 : public ArrayList<String> getLevelNames()
 	// 4. 등급별 고객 출력 : public ArrayList<CustomerDto> levelNameGetCustomers(String levelName)
 	// 5. 전체 출력 : public ArrayList<CustomerDto> getCustomers()
