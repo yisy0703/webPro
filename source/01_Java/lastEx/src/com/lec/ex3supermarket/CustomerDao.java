53b396b5dbc38032acdfa3741693735580b13b73
yisy0703 <yisy0703@naver.com>
2023-01-09 13:36:38 +0900

    JDBC

--- source/01_Java/lastEx/src/com/lec/ex3supermarket/CustomerDao.java
+++ source/01_Java/lastEx/src/com/lec/ex3supermarket/CustomerDao.java
@@ -3,7 +3,9 @@ package com.lec.ex3supermarket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CustomerDao {
 	private String driver = "oracle.jdbc.driver.OracleDriver";
@@ -49,14 +51,37 @@ public class CustomerDao {
 	}
 	public int insertCustomer(String ctel, String cname) {
 		int result = FAIL;
-		//
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		String sql = "INSERT INTO CUSTOMER (CID, CTEL, CNAME)" + 
+						"  VALUES (CUSTOMER_CID_SEQ.NEXTVAL, ?, ?) ";
+		try {
+			conn = DriverManager.getConnection(url, "scott", "tiger");
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, ctel);
+			pstmt.setString(2, cname);
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}finally {
+			try {
+				if(pstmt!=null) pstmt.close();
+				if(conn !=null) conn.close();
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
 		return result;
 	}
-	
-  
-	
 	// 2. 폰뒤4자리(풀번호) 검색  : 
-	// public ArrayList<CustomerDto> ctelGetCustomers(String searchTel)
+	public ArrayList<CustomerDto> ctelGetCustomers(String searchTel){
+		ArrayList<CustomerDto> dtos = new ArrayList<CustomerDto>();
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		ResultSet         rs    = null;
+		String sql = "";
+		return dtos;
+	}
 	
 	// 3. 물품구입 : public int buy(int cid, int price)
 	
