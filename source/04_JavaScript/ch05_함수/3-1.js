005ca68989c998b0024d789bbdede29c6ff5d004
yisy0703 <yisy0703@naver.com>
2023-01-20 17:47:49 +0900

    js

---
+++ source/04_JavaScript/ch05_함수/3-1.js
@@ -0,0 +1,6 @@
+function funVar(){
+  alert('함수 A');
+}
+function theGood(){
+  alert('xxx');
+}
---
+++ source/04_JavaScript/ch05_함수/3.js
@@ -0,0 +1,7 @@
+// 3.js
+function good(){
+  alert('goooooooooooo');
+}
+function funVar(){
+  alert('함수 B');
+}
