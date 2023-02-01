20d2413ecd6aea91e1f6f72a90540f038026e53a
yisy0703 <yisy0703@naver.com>
2023-02-01 11:32:51 +0900

    react

--- source/05_react/ch01_OOP_ES6/4.inheritance_super.js
+++ source/05_react/ch01_OOP_ES6/4.inheritance_super.js
@@ -10,3 +10,19 @@ class Person{
 } // class
 var hong = new Person('홍길동', 100, 60);
 console.log('홍길동의 sum : ', hong.sum());
+
+class PersonPlus extends Person{
+  constructor(name, first, second, third){ // 생성자 함수는 무조건 constructor
+    super(name, first, second); // 부모클래스 생성자 함수
+    this.third = third;
+  }
+  sum(){
+    return this.first + this.second + this.third;
+  }
+  avg(){
+    return this.sum()/3;
+  }
+}
+var son = new Person('손흥민', 100,100,100);
+console.log('손 sum : ', son.sum());
+console.log('손 avg : ', son.avg());
