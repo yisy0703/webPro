8607ef8fbe2d74c2fbb4bb9a3bb8904ca245b043
yisy0703 <yisy0703@naver.com>
2023-02-02 10:24:24 +0900

    react

--- source/05_react/ch03_component/src/App.js
+++ source/05_react/ch03_component/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import React, {Component} from 'react';
-import Subject from './components/Subject';
+//import Subject from './components/Subject';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
 
 class App extends Component{
   constructor(props){
     super(props);
     this.state = {
+      mode : 'read',
       subject : {title:'WEB', sub:'World wide web!'},
       contents : [
         {id:1, title:'HTML', desc:'HTML is HyperText Markup Langauge.'},
@@ -17,14 +18,37 @@ class App extends Component{
       welcome : {title:'WELCOME', desc:'Hello, React!!!'},
     };
   }
+
   render(){
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div>
-        <Subject title={this.state.subject.title} 
-                sub={this.state.subject.sub}></Subject> 
+        {/* <Subject title={this.state.subject.title} 
+                sub={this.state.subject.sub}></Subject>  */}
+        <header className='subject_h'>
+          <h1>
+            <a href="/" onClick={function(event){
+                event.preventDefault(); // a 태그의 기본 event기능을 막음
+                //this.state.mode = 'welcome'; state값은 바로 변경해도 화면에 반영 안 함
+                this.setState({
+                  mode : 'welcome',
+                });
+            }.bind(this)}>
+              {this.state.subject.title}
+            </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
-        <Content title={this.state.welcome.title} 
-                desc={this.state.welcome.desc}></Content>
+        <ReadContent title={_title} 
+                desc={_desc}></ReadContent>
       </div>
     );
   }
--- source/05_react/ch03_component/src/components/Subject.js
+++ source/05_react/ch03_component/src/components/Subject.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 import './Subject.css';
 class Subject extends Component{
   render(){
+    // this.props.title = 'React'; readOnly
     return (
       <header className='subject_h'>
-        <h1>{this.props.title}</h1>
+        <h1><a href="/">{this.props.title}</a></h1>
         {this.props.sub}
       </header>
     );
