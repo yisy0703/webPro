e2d266c08f9832b5e698c52b28f02651c447c1f4
yisy0703 <yisy0703@naver.com>
2023-02-02 11:43:13 +0900

    react

---
+++ source/05_react/ch04_crud/src/components/Subject.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './Subject.css';
+class Subject extends Component{
+  render(){
+    console.log('subject render');
+    // this.props.title = 'React'; readOnly
+    return (
+      <header className='subject_h'>
+        <h1>
+          <a href="/" onClick={function(event){
+            event.preventDefault();
+            this.props.onChangePage();
+          }.bind(this)}>
+            {this.props.title}
+          </a>
+        </h1>
+        {this.props.sub}
+      </header>
+    );
+  }
+}
+export default Subject;
---
+++ source/05_react/ch04_crud/src/components/TOC.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+class TOC extends Component{
+  shouldComponentUpdate(newProps){ // render 실행 전 newProps(새props값), this.props(현재값)
+    // return false; // render 무조건 실행하지 않도록
+    // return true; // render 무조건 실행
+    if(newProps.data === this.props.data){
+      return false;
+    }
+    return true;
+  }
+  render(){
+    console.log('TOC render');
+    var lists = [];
+    // this.props.data를 받아 <li><a href="1.html">HTML</a></li> 형태의 배열로 lists 
+    var data = this.props.data;
+    for(var i=0 ; i < data.length ; i++){
+      lists.push(
+        <li key={data[i].id}>
+          <a href={data[i].id+".html"} 
+            data-id={data[i].id}
+            onClick={function(event){
+            event.preventDefault();
+            //console.log(event.target.dataset.id);
+            //debugger;
+            this.props.onChangePage(event.target.dataset.id);
+          }.bind(this)}>
+            {data[i].title}          
+          </a>
+        </li>
+      );
+    }
+    return (
+      <nav>
+        <ul>
+          {lists}
+        </ul>
+      </nav>
+    );
+  }
+}
+export default TOC;
