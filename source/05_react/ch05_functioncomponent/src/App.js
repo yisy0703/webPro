64bb684ef48db80148a48f789f5634feec345fca
yisy0703 <yisy0703@naver.com>
2023-02-03 13:14:18 +0900

    react_function

--- source/05_react/ch05_functioncomponent/src/App.js
+++ source/05_react/ch05_functioncomponent/src/App.js
@@ -2,38 +2,56 @@ import Subject from './components/Subject';
 import TOC from './components/TOC';
 import Control from './components/Control';
 import ReadContent from './components/ReadContent';
+import React, {useState} from 'react';
 
 function App() {
-  let mode = 'read';
+  // let mode = 'read';
+  // const _mode = useState('read');
+  // let mode = _mode[0];
+  // let setMode = _mode[1];
+  let [mode, setMode] = useState('read');
   let maxId = 3; // 현재 contents(삭제되었을 수도 있는)의 가장 큰 id. create모드에서 사용
-  let selectedId = 2;
-  let contents = [
+  let [selectedId, setSelectedId] = useState(2);
+  let [contents, setContents] = useState([
     {id:1, title:'HTML', body:'HTML is HyperText Markup Langauge.'},
     {id:2, title:'CSS', body:'CSS is for design.'},
     {id:3, title:'JavaScript', body:'JavaScript is for interactive.'}
-  ];
+  ]);
   let subject = {title : 'WEB', sub:'World Wide Web!'};
   let welcome = {title:'WELCOME', body:'Hello, React!!! !!!'};
   let _title, _body, _article = null;
+
   if(mode === 'welcome'){
+    console.log(mode);
     _title = welcome.title;
     _body = welcome.body;
     _article = <ReadContent title={_title} body={_body}></ReadContent>
   }else if(mode === 'read'){
-    _title = contents[0].title;
-    _body = contents[0].body;
+    
+    for(var i=0 ; i<contents.length ; i++){
+      if(contents[i].id === selectedId){
+        _title = contents[i].title;
+        _body = contents[i].body;
+        break;
+      }
+    }
     _article = <ReadContent title={_title} body={_body}></ReadContent>
   }
   return (
     <>
       <Subject title={subject.title} sub={subject.sub}
               onChangeMode={()=>{
-                mode = 'welcome';
+                //mode = 'welcome';
+                setMode('welcome');
               }}
       ></Subject>
-      <TOC data={contents}></TOC>
+      <TOC data={contents} onChangeMode={(_id)=>{
+        //selectedId = _id;
+        setSelectedId(_id);
+        setMode('read');
+      }}></TOC>
       <Control></Control>
-      <ReadContent title="CSS" body="CSS is for design."></ReadContent>
+      {_article}
     </>
   );
 }
--- source/05_react/ch05_functioncomponent/src/components/TOC.js
+++ source/05_react/ch05_functioncomponent/src/components/TOC.js
@@ -5,8 +5,13 @@ const TOC = props => {
     lists.push(
       <li key={data[idx].id}>
         <a href={data[idx].id+'.html'} 
-            data-id={data[idx].id}
-            id={data[idx].id}>
+            id={data[idx].id}
+            onClick={ event => {
+              event.preventDefault();
+              //console.log(event.target.id); //event.target:이벤트가 발생된 태그 객체
+              //debugger;
+              props.onChangeMode(Number(event.target.id));
+            }}>
           {data[idx].title}
         </a>
       </li>
