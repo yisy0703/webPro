70569dbb564afcad6176308bd864badf2f9e6ddf
yisy0703 <yisy0703@naver.com>
2023-02-03 10:12:00 +0900

    react_function

--- source/05_react/ch05_functioncomponent/src/App.js
+++ source/05_react/ch05_functioncomponent/src/App.js
@@ -1,26 +1,46 @@
+const Subject = () => {
+  return (
+    <header>
+      <h1>WEB</h1>
+      World wide web!
+    </header>
+  );
+};
+const TOC = () => {
+  return (
+    <nav>
+      <ol>
+        <li><a href="1.html">HTML</a></li>
+        <li><a href="2.html">CSS</a></li>
+        <li><a href="3.html">JavaScript</a></li>
+      </ol>
+    </nav>
+  );
+};
+const Control = () => {
+  return (
+    <ul class="Control">
+      <li><button>create</button></li>
+      <li><button>update</button></li>
+      <li><button>delete</button></li>
+    </ul>
+  );
+};
+const ReadContent = () => {
+  return (
+    <article>
+      <h2>HTML</h2>
+      HTML is HyperText Markup Langauge.
+    </article>
+  );
+};
 function App() {
   return (
     <>
-      <header>
-        <h1>WEB</h1>
-        World wide web!
-      </header>
-      <nav>
-        <ol>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ol>
-      </nav>
-      <ul class="Control">
-        <li><button>create</button></li>
-        <li><button>update</button></li>
-        <li><button>delete</button></li>
-      </ul>
-      <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Langauge.
-      </article>
+      <Subject></Subject>
+      <TOC></TOC>
+      <Control></Control>
+      <ReadContent></ReadContent>
     </>
   );
 }
