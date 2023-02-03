5f7289757e3c040b9a43a2dddc35c5c587e059d7
yisy0703 <yisy0703@naver.com>
2023-02-03 10:04:43 +0900

    react_function

---
+++ source/05_react/ch05_functioncomponent/src/App.js
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>WEB</h1>
+        World wide web!
+      </header>
+      <nav>
+        <ol>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ol>
+      </nav>
+      <ul class="Control">
+        <li><button>create</button></li>
+        <li><button>update</button></li>
+        <li><button>delete</button></li>
+      </ul>
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Langauge.
+      </article>
+    </>
+  );
+}
+
+export default App;
