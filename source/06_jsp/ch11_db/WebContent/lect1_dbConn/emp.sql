868807363cd4769e6ab3dabba92338437dc08d82
yisy0703 <yisy0703@naver.com>
2023-02-15 17:58:43 +0900

    jsp_db

--- source/06_jsp/ch11_db/WebContent/lect1_dbConn/emp.sql
+++ source/06_jsp/ch11_db/WebContent/lect1_dbConn/emp.sql
@@ -1,1 +1,5 @@
-SELECT * FROM EMP; -- 실행하고자 하는 SQL문을 블럭잡고 alt+x로 실행
+SELECT * FROM EMP; -- 실행하고자 하는 SQL문을 블럭잡고 alt+x로 실행 ex2_oracle.jsp
+-- ex3
+SELECT * FROM DEPT;
+SELECT * FROM EMP WHERE DEPTNO LIKE '%'||'10';
+
