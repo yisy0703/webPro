b32e66d143e51d8d00e3e0ba462daf3b0425d347
yisy0703 <yisy0703@naver.com>
2023-02-16 12:18:28 +0900

    jsp_db

--- source/06_jsp/ch11_db/WebContent/lect1_dbConn/emp.sql
+++ source/06_jsp/ch11_db/WebContent/lect1_dbConn/emp.sql
@@ -3,3 +3,5 @@ SELECT * FROM EMP; -- 실행하고자 하는 SQL문을 블럭잡고 alt+x로 실
 SELECT * FROM DEPT;
 SELECT * FROM EMP WHERE DEPTNO LIKE '%'||'10';
 
+SELECT E.*, DNAME, LOC FROM EMP E, DEPT D WHERE D.DEPTNO=E.DEPTNO and ENAME LIKE '%'||trim(UPPER('   s'))||'%';
+
