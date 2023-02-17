2456438dc52284cb0c5c98aab5b3a8c347c5a0ac
yisy0703 <yisy0703@naver.com>
2023-02-17 10:05:36 +0900

    jsp_member

--- source/06_jsp/ch12_member/src/com/lec/member/MemberDao.java
+++ source/06_jsp/ch12_member/src/com/lec/member/MemberDao.java
@@ -5,5 +5,31 @@ public class MemberDao {
 	public static final int MEMBER_EXISTENT = 0; // 중복된 ID일 때 리턴값
 	public static final int MEMBER_NONEXISTENT = 1; // 사용가능한 ID일 때 리턴값
 	public static final int LOGIN_SUCCESS = 1; // 로그인 성공시 리턴값
-	public static final int LOGIN_FAIL_PW = 0; // 로그인시 pw오류일때 리턴값
+	public static final int LOGIN_FAIL_PW = 0; // 로그인시 pw오류일 때 리턴값
+	public static final int LOGIN_FAIL_ID = -1;// 로그인시 id오류일 때 리턴값
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url    = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	// 싱글톤
+	private static MemberDao instance = new MemberDao(); // 자기가 자기 클래스형 객체를 참조
+	public static MemberDao getInstance() {
+//		if(instance==null) {
+//			instance = new MemberDao();
+//		}
+		return instance;
+	}
+	private MemberDao() {
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
+
+
+
+
+
+
+
+
