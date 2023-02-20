fa9b536f1c840d22ad634edfda20ed11dc4cc57e
yisy0703 <yisy0703@naver.com>
2023-02-20 12:48:55 +0900

    jsp_member

--- source/06_jsp/ch12_member/WebContent/db/customer.sql
+++ source/06_jsp/ch12_member/WebContent/db/customer.sql
@@ -21,11 +21,22 @@ INSERT INTO CUSTOMER (CID, CPW, CNAME, CTEL, CEMAIL, CADDRESS, CBIRTH, CGENDER)
         TO_DATE('1995-12-12','YYYY-MM-DD'), 'm');
         
 -- 3. 로그인 : public int loginCheck(String cid, String cpw)
+SELECT CID, CPW FROM CUSTOMER WHERE CID='aaa' AND CPW='111';
 
 -- 4. cid로 DTO 가져오기 : public CustomerDto getCustomer(String cid)
+SELECT * FROM CUSTOMER WHERE CID='aaa';
 
 -- 5. 정보수정 : public int modifyCustomer(CustomerDto dto)
-
+UPDATE CUSTOMER SET CPW = '111',
+                CNAME='홍길동',
+                CTEL = '02-111-1111',
+                CEMAIL='GIL@H.COM',
+                CADDRESS='제주도',
+                CBIRTH = TO_DATE('1990-12-12','YYYY-MM-DD'),
+                CGENDER = 'f'
+          WHERE CID='aaa';
+SELECT * FROM CUSTOMER;
+COMMIT;
 
 
 
