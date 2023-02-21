e34aa1cb93195c3464a9e4f458d9747e44e14aae
yisy0703 <yisy0703@naver.com>
2023-02-21 15:12:50 +0900

    jsp_board

--- source/06_jsp/ch13_board/WebContent/db/board.sql
+++ source/06_jsp/ch13_board/WebContent/db/board.sql
@@ -1,4 +1,5 @@
 -- BOARD TABLE DROP & CREATE
+DROP TABLE BOARD;
 CREATE TABLE BOARD(
   NUM     NUMBER(5,0) PRIMARY KEY, -- 글번호
   WRITER  VARCHAR2(30) NOT NULL,   -- 글작성자
@@ -13,6 +14,17 @@ CREATE TABLE BOARD(
   IP      VARCHAR2(30) NOT NULL,   -- 글 작성시 컴퓨터 IP 주소
   RDATE   DATE DEFAULT SYSDATE     -- 글쓴 시점(날짜+시간)
 );
+-- 1. 글갯수
+
+-- 2. 글목록(글그룹이 최신인 글이 위로)
+
+-- 3. 글쓰기(원글쓰기) - 글쓴이, 글제목, 글본문, 이메일, PW
+
+-- 4. 글번호로 글상세보기 내용(DTO) 가져오기
+
+-- 5. 조회수 올리기
+
+-- 6. 글삭제(비밀번호를 맞게 입력한 경우만 삭제)
 
 
 
@@ -21,4 +33,5 @@ CREATE TABLE BOARD(
 
 
 
--- 
+
+
