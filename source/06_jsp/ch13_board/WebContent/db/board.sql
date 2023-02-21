1c38a5b8df5405100014da0b4e74b373e1255c7f
yisy0703 <yisy0703@naver.com>
2023-02-21 14:47:45 +0900

    jsp_board

---
+++ source/06_jsp/ch13_board/WebContent/db/board.sql
@@ -0,0 +1,21 @@
+-- BOARD TABLE DROP & CREATE
+CREATE TABLE BOARD(
+  NUM     NUMBER(5,0) PRIMARY KEY, -- 글번호
+  WRITER  VARCHAR2(30) NOT NULL,   -- 글작성자
+  SUBJECT VARCHAR2(100) NOT NULL,  -- 글제목
+  CONTENT VARCHAR2(4000) NOT NULL, -- 본문
+  EMAIL   VARCHAR2(30),            -- 작성자 이메일
+  READCOUNT NUMBER(5) DEFAULT 0,   -- 글 HIT수 (조회수)
+  PW      VARCHAR2(30) NOT NULL,   -- 글 삭제시 쓸 비밀번호
+  IP      VARCHAR2(30) NOT NULL,   -- 글 작성시 컴퓨터 IP 주소
+  RDATE   DATE DEFAULT SYSDATE     -- 글쓴 시점(날짜+시간)
+);
+
+
+
+
+
+
+
+
+-- 
