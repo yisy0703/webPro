c0b74c83445e6cad98bbd0846398dc91f48585f4
yisy0703 <yisy0703@naver.com>
2023-02-21 15:03:36 +0900

    jsp_board

--- source/06_jsp/ch13_board/WebContent/db/board.sql
+++ source/06_jsp/ch13_board/WebContent/db/board.sql
@@ -7,9 +7,9 @@ CREATE TABLE BOARD(
   EMAIL   VARCHAR2(30),            -- 작성자 이메일
   READCOUNT NUMBER(5) DEFAULT 0,   -- 글 HIT수 (조회수)
   PW      VARCHAR2(30) NOT NULL,   -- 글 삭제시 쓸 비밀번호
-  
-  
-  
+  REF     NUMBER(5,0) NOT NULL,    -- 글그룹(원글의 경우 글번호로/답변글일경우 원글의 REF로)
+  RE_STEP NUMBER(3) NOT NULL,      -- 글그룹내 출력 순서(원글 0)
+  RE_INDENT NUMBER(3) NOT NULL,    -- 글 LIST 출력시 글 제목 들여쓰기 정도(원글0)
   IP      VARCHAR2(30) NOT NULL,   -- 글 작성시 컴퓨터 IP 주소
   RDATE   DATE DEFAULT SYSDATE     -- 글쓴 시점(날짜+시간)
 );
