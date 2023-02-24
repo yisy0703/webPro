6c05562e9bf1bb7046878ecf1c6ec406b7f8ccdb
yisy0703 <yisy0703@naver.com>
2023-02-24 10:50:44 +0900

    jsp_fileUpload

---
+++ source/06_jsp/ch14_fileUp/src/com/lec/book/BookDto.java
@@ -0,0 +1,11 @@
+package com.lec.book;
+
+public class BookDto {
+	private int bid;
+	private String btitle;
+	private int bprice;
+	private String bimage1;
+	private String bimage2;
+	private String bcontent;
+	private int    bdiscount;
+}
