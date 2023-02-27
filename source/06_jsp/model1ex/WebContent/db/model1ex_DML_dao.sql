973928ea519a0c875d38921bfeaeda1a6b30b65c
yisy0703 <yisy0703@naver.com>
2023-02-27 12:00:49 +0900

    jsp_model1ex

--- source/06_jsp/model1ex/WebContent/db/model1ex_DML_dao.sql
+++ source/06_jsp/model1ex/WebContent/db/model1ex_DML_dao.sql
@@ -59,33 +59,39 @@ INSERT INTO FILEBOARD (fNUM, CID, fSUBJECT, fCONTENT, fFILENAME, fPW, fREF, fRE_
 SELECT * FROM FILEBOARD WHERE fREF = 4 ORDER BY FRE_STEP; -- 4번 그룹 확인
 
 -- 6. 글 상세보기(fnum으로 dto가져오기)
-SELECT F.*, CNAME, CEMAIL FROM FILEBOARD F, CUSTOMER C WHERE F.CID=C.CID AND fNUM=2;
+SELECT F.*, CNAME, CEMAIL FROM FILEBOARD F, CUSTOMER C WHERE F.CID=C.CID AND fNUM=4;
 
 -- 7. 조회수 올리기
-UPDATE FILEBOARD SET fHIT = fHIT+1 WHERE fNUM=5;
+UPDATE FILEBOARD SET fHIT = fHIT+1 WHERE fNUM=4;
+
 -- 8. 글 수정하기
 UPDATE FILEBOARD SET fSUBJECT = '수정된 제목',
                     fCONTENT = '수정된 본문',
                     fFILENAME = NULL,
                     fPW = '111',
                     fIP = '192.168.10.30'
-                WHERE fNUM=1;
+                WHERE fNUM=4;
+commit;                
 -- 9. 글 삭제하기
-DELETE FROM FILEBOARD WHERE fNUM=1 AND fPW='111';
+DELETE FROM FILEBOARD WHERE fNUM=1 AND fPW='1';
+rollback;
 
 -- BookDao(top-N 리스트, 책갯수, 책등록, 상세보기)
--- 책등록
+-- 1. 책등록
 INSERT INTO BOOK (BID, BTITLE, BPRICE, BIMAGE1, BIMAGE2, BCONTENT, BDISCOUNT)
     VALUES (BOOK_SEQ.NEXTVAL, '이것은 자바다', 30000, 'noImg.png','NOTHING.JPG','책설명',20);
 
 -- 책목록(전체list) -- 신간 도서 순으로 출력
 SELECT * FROM BOOK ORDER BY BRDATE DESC;
+
 -- 책목록(top-N구문)
 SELECT * 
     FROM (SELECT ROWNUM RN, A.* FROM (SELECT * FROM BOOK ORDER BY BRDATE DESC) A)
     WHERE RN BETWEEN 2 AND 3;
+
 -- 등록된 책 갯수
 SELECT COUNT(*) CNT FROM BOOK;
+
 -- 책 상세보기(bID로 dto가져오기)
 SELECT * FROM BOOK WHERE BID=1;
 
