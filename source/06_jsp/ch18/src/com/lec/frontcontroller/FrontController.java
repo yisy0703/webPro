5072b920dd522078d2e24c8180c6313b5d9a5a70
yisy0703 <yisy0703@naver.com>
2023-03-07 12:16:38 +0900

    jsp_mvc

---
+++ source/06_jsp/ch18/src/com/lec/frontcontroller/FrontController.java
@@ -0,0 +1,30 @@
+package com.lec.frontcontroller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		actionDo(request, response);
+	}
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+		response.setContentType("text/html; charset=UTF-8");
+		response.getWriter().append("<h1>FrontController</h1>");
+		// 무슨 요청이 들어왔는지 판별하고 해당 요청을 수행하고 view로 forward한다
+	}
+}
+
+
+
+
+
+
