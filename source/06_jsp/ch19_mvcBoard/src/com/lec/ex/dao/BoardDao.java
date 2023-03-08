423ea737833429e1502467b6b882fcb33de45622
yisy0703 <yisy0703@naver.com>
2023-03-08 12:19:12 +0900

    mvc_board

--- source/06_jsp/ch19_mvcBoard/src/com/lec/ex/dao/BoardDao.java
+++ source/06_jsp/ch19_mvcBoard/src/com/lec/ex/dao/BoardDao.java
@@ -13,6 +13,8 @@ import javax.sql.DataSource;
 
 import com.lec.ex.dto.BoardDto;
 public class BoardDao {
+	private final int SUCCESS = 1;
+	private final int FAIL    = 0;
 	private DataSource ds;
 	public BoardDao() {
 		try {
@@ -66,7 +68,62 @@ public class BoardDao {
 		return dtos;
 	}
 	// 2. 전체 글 갯수
-	// 3. 원글 쓰기
+	public int getBoardTotCnt() {
+		int totCnt = 0;
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		ResultSet         rs    = null;
+		String sql = "SELECT COUNT(*) FROM MVC_BOARD";
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			rs.next();
+			totCnt = rs.getInt(1);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				if(rs    != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn  != null) conn.close();
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			} 
+		}
+		return totCnt;
+	}
+	// 3. 원글 쓰기 (bname, btitle, bcontent, bip)
+	public int write(String bname, String btitle, String bcontent, String bip) {
+		int result = FAIL;
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		String sql = "INSERT INTO MVC_BOARD (BID, BNAME, BTITLE, BCONTENT, "
+				+ "							BGROUP, BSTEP, BINDENT, BIP)" + 
+				" VALUES (MVC_BOARD_SEQ.NEXTVAL, ?, ?, ?, "
+				+ "							MVC_BOARD_SEQ.CURRVAL, 0, 0, ?)";
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, bname);
+			pstmt.setString(2, btitle);
+			pstmt.setString(3, bcontent);
+			pstmt.setString(4, bip);
+			pstmt.executeUpdate();
+			result = SUCCESS;
+			System.out.println("원글쓰기 성공");
+		} catch (SQLException e) {
+			System.out.println(e.getMessage() + " 원글쓰기 실패 :");
+		} finally {
+			try {
+				if(pstmt != null) pstmt.close();
+				if(conn  != null) conn.close();
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			} 
+		}
+		return result;
+	}
 	// 4. bID로 조회수 1 올리기
 	// 5. bID로 DTO가져오기 (글상세보기) -- 조회수 올리고 dto가져오기
 	// 6. bID로 DTO가져오기 (글수정VIEW, 답변글VIEW) -- dto 가져오기
