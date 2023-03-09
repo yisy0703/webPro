28881beea7ffc661eb3cc797082d5e35841ea725
yisy0703 <yisy0703@naver.com>
2023-03-09 14:02:36 +0900

    mvc_board

---
+++ source/06_jsp/ch19_mvcMember/WebContent/db/mvc_member.sql
@@ -0,0 +1,13 @@
+DROP TABLE MVC_MEMBER;
+CREATE TABLE MVC_MEMBER ();
+
+-- DUMMY DATA
+
+-- DAO 에 들어갈 QUERY
+  -- 1. ID중복체크
+  
+  -- 2. JOIN
+  
+  -- 3. LOGIN CHECK
+  
+  -- 4. mID로 MemberDto 가져오기
