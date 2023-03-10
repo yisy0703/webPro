29cafb06acfcf12abd9892933d900c54d4e44260
yisy0703 <yisy0703@naver.com>
2023-03-10 11:13:51 +0900

    mvc_board

--- source/06_jsp/ch19_mvcMember/WebContent/db/mvcmemberDao.sql
+++ source/06_jsp/ch19_mvcMember/WebContent/db/mvcmemberDao.sql
@@ -9,7 +9,21 @@ COMMIT;
 SELECT * FROM MVC_MEMBER WHERE mID='aaa' and mPW='1';
 -- (4) mid로 dto가져오기(로그인 성공시 session에 넣기 위함)
 SELECT * FROM MVC_MEMBER WHERE MID='aaa';
-
+-- (5) 회원정보 수정
+UPDATE MVC_MEMBER 
+  SET MPW='1',
+      MNAME='송중기',
+      MEMAIL='SONG@S.COM',
+      MPHOTO = 'NOIMG.JPG',
+      MBIRTH = '1995-12-12',
+      MADDRESS = '서대문'
+  WHERE MID='song';
+-- (6) 회원 리스트(top-n)
+SELECT * FROM MVC_MEMBER ORDER BY MRDATE DESC; -- 신입회원순
+SELECT *
+  FROM (SELECT ROWNUM RN, A.* FROM (SELECT * FROM MVC_MEMBER ORDER BY MRDATE DESC) A)
+  WHERE RN BETWEEN 4 AND 6;
+-- (7) 전체 등록된 회원수
 
 
 
