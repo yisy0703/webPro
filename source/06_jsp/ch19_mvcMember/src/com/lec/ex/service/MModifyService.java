2120bc6ccdd6721365060cdf519e8130045e0efb
yisy0703 <yisy0703@naver.com>
2023-03-10 15:03:59 +0900

    mvc_board

--- source/06_jsp/ch19_mvcMember/src/com/lec/ex/service/MModifyService.java
+++ source/06_jsp/ch19_mvcMember/src/com/lec/ex/service/MModifyService.java
@@ -1,10 +1,20 @@
 package com.lec.ex.service;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.Date;
 import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import com.lec.ex.dao.MemberDao;
+import com.lec.ex.dto.MemberDto;
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 public class MModifyService implements Service {
@@ -26,6 +36,14 @@ public class MModifyService implements Service {
 			// 파라미터 받아 DB에 수정 적용
 			String dbMpw = mRequest.getParameter("dbMpw");
 			String dbMphoto = mRequest.getParameter("dbMphoto");
+			// hidden으로 넘어온 dbMpw, dbMphoto가 없을 경우
+//			HttpSession session = request.getSession();
+//			String sessionMpw=null, sessionMphoto=null;
+//			MemberDto sesionMember = (MemberDto)session.getAttribute("member");
+//			if(member!=null) {
+//				sessionMpw = sesionMember.getMpw();
+//				sessionMphoto = sesionMember.getMphoto();
+//			}
 			String mid      = mRequest.getParameter("mid");
 			String oldMpw   = mRequest.getParameter("oldMpw");
 			if(!oldMpw.equals(dbMpw)) {
@@ -33,13 +51,68 @@ public class MModifyService implements Service {
 				return;
 			}
 			String mpw = mRequest.getParameter("mpw");
-			
+			if(mpw.equals("")) { // 정보 수정시 새비밀번호를 입력하지 않을 경우, 현비밀번호(dbMpw)로 
+				mpw = dbMpw;
+			}
+			String mname = mRequest.getParameter("mname");
+			String memail = mRequest.getParameter("memail");
+			// 정보 수정시 사진첨부 안 하면, 기존의 사진(dbMphoto)으로
+			mphoto = mphoto==null? dbMphoto : mphoto;
+			String mbirthStr = mRequest.getParameter("mbirth");
+			Date mbirth = null;
+			if(!mbirthStr.equals("")) {
+				mbirth = Date.valueOf(mbirthStr);
+			}
+			String maddress = mRequest.getParameter("maddress");
+			// 회원정보 수정
+			MemberDao mDao = MemberDao.getInstance();
+			MemberDto mDto = new MemberDto(mid, mpw, mname, memail, mphoto, mbirth, maddress, null);
+			result = mDao.modifyMember(mDto); 
+			if(result == MemberDao.SUCCESS) { // 수정 성공시 세션도 수정
+				HttpSession session = request.getSession();
+				session.setAttribute("member", mDto);
+				request.setAttribute("modifyResult", "회원정보 수정 성공");
+			}else { // 수정 실패시
+				request.setAttribute("modifyErrorMsg", "회원정보 수정 실패");
+			}
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
 		//서버에 저장된 파일을 소스 폴더로 복사(파일명이 NOIMG.JPG거나 result가 FAIL이 아닐 경우) 
-	}
+		File serverFile = new File(path+"/"+mphoto);
+		if(!mphoto.equals("NOIMG.JPG") && result==MemberDao.SUCCESS) {
+			InputStream is = null;
+			OutputStream os = null;
+			try {
+				is = new FileInputStream(serverFile);
+				os = 
+				new FileOutputStream("D:/webPro/source/06_jsp/ch19_mvcMember/WebContent/memberPhotoUp/"
+											+ mphoto);
+				byte[] bs = new byte[(int)serverFile.length()];
+				while(true) {
+					int readByteCnt = is.read(bs);
+					if(readByteCnt==-1) break;
+					os.write(bs, 0, readByteCnt);
+				}
+				System.out.println("첨부된 파일("+mphoto+") 복사 완료");
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			} finally {
+				try {
+					if(os!=null) os.close();
+					if(is!=null) is.close();
+				}catch (IOException e) {
+					// TODO: handle exception
+				}
+			}// try-catch-finally
+		}//if-파일 복사
+	}// execute()
 }
+
+
+
+
+
 
 
 
