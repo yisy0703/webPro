5e1f3f478c17707e9597ec656ac8f236036da744
yisy0703 <yisy0703@naver.com>
2023-03-14 16:10:22 +0900

    jQuery

--- source/07_jQuery/ch03_ajax/src/com/lec/ex/dao/ProductDao.java
+++ source/07_jQuery/ch03_ajax/src/com/lec/ex/dao/ProductDao.java
@@ -1,4 +1,9 @@
 package com.lec.ex.dao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -19,4 +24,41 @@ public class ProductDao {
 			System.out.println(e.getMessage());
 		}
 	}
+	// 재고량 return
+	public int getProductStock(String pname) {
+		int pStock = -1;
+		Connection        conn  = null;
+		PreparedStatement pstmt = null;
+		ResultSet         rs    = null;
+		String sql = "SELECT PSTOCK FROM PRODUCT WHERE PNAME=?";
+		try {
+			conn = ds.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, pname);
+			rs = pstmt.executeQuery();
+			if(rs.next()) { // DB에 있는 상품명
+				pStock = rs.getInt("pstock");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				if(rs   !=null) rs.close();
+				if(pstmt!=null) pstmt.close();
+				if(conn !=null) conn.close();
+			} catch (SQLException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+		return pStock;
+	}
 }
+
+
+
+
+
+
+
+
+
