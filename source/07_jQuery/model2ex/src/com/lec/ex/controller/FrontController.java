b3ba27fcee195ae9955d09e4681c90cbd320b2df
yisy0703 <yisy0703@naver.com>
2023-03-15 13:32:16 +0900

    model2ex

---
+++ source/07_jQuery/model2ex/src/com/lec/ex/controller/FrontController.java
@@ -0,0 +1,55 @@
+package com.lec.ex.controller;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.lec.ex.service.Service;
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		actionDo(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		actionDo(request, response);
+	}
+	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String command = uri.substring(conPath.length());
+		String viewPage = null;
+		Service service = null;
+		if(command.equals("/main.do")) { // 첫화면
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * 
+		 * * * * * * * * * * member 관련 요청  * * * * * * * * * *
+		  * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		}else if(command.equals("/joinView.do")) {
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * 
+		 * * * * * * * * * * admin 관련 요청  * * * * * * * * * *
+		 * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		}
+		
+		/* * * * * * * * * * * * * * * * * * * * * * * * * * * 
+		 * * * * * * * * 파일첨부 게시판 관련 요청  * * * * * * * * * *
+		 * * * * * * * * * * * * * * * * * * * * * * * * * * */
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
