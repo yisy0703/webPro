669ca68a12bb0eac5b00c61e8c8c3b004c77808f
yisy0703 <yisy0703@naver.com>
2023-03-16 13:42:44 +0900

    model2ex

--- source/07_jQuery/model2ex/WebContent/db/글삭제시답변들까지함께삭제.sql
+++ source/07_jQuery/model2ex/WebContent/db/글삭제시답변들까지함께삭제.sql
@@ -1,2 +1,7 @@
 -- source/06_jsp/model2ex 프로젝트는 글삭제시 해당글만 삭제
 -- source/07_jQuery/model2ex 프로젝트는 글삭제시 답변글들까지 삭제
+DROP TABLE MEMO;
+CREATE TABLE MEMO (
+  ID NUMBER(4) PRIMARY KEY,
+  TITLE VARCHAR2(100) NOT NULL
+);
