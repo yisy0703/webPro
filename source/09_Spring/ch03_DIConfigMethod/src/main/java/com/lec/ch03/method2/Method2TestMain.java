534daafef4cce0db167a4052cd5fc109b12d21b2
yisy0703 <yisy0703@naver.com>
2023-04-04 15:04:26 +0900

    spring DI

---
+++ source/09_Spring/ch03_DIConfigMethod/src/main/java/com/lec/ch03/method2/Method2TestMain.java
@@ -0,0 +1,30 @@
+package com.lec.ch03.method2;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import com.lec.ch03.Student;
+public class Method2TestMain {
+	public static void main(String[] args) {
+		AbstractApplicationContext ctx
+		  //= new GenericXmlApplicationContext("xml경로", "~");
+		  = new AnnotationConfigApplicationContext(Method2_ApplicationConfig.class,
+				  								Method2_ApplicationConfig2.class);
+		Student student1 = ctx.getBean("student1", Student.class);
+		Student student2 = (Student) ctx.getBean("student2");
+		System.out.println(student1);
+		System.out.println(student2);
+		ctx.close();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ source/09_Spring/ch04_BeanLifeCycle/src/main/java/com/lec/ch04/ex1/StudentInfo.java
@@ -0,0 +1,12 @@
+package com.lec.ch04.ex1;
+import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Data;
+@Data
+public class StudentInfo {
+	@Autowired
+	private Student student;
+//	@Autowired
+//	public void setStudent(Student student) {
+//		this.student = student;
+//	}	
+}
