8a0f9ff03ec310b9ed56747d4bb8c4f633263901
yisy0703 <yisy0703@naver.com>
2023-04-10 14:37:43 +0900

    springFileupload

--- source/09_Spring/ch13_fileupload/src/main/java/com/lec/ch13/controller/FileController.java
+++ source/09_Spring/ch13_fileupload/src/main/java/com/lec/ch13/controller/FileController.java
@@ -1,11 +1,40 @@
 package com.lec.ch13.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.lec.ch13.service.FileUpService;
 @Controller
 public class FileController {
+	@Autowired
+	private FileUpService fService;
 	@RequestMapping(value="fileup", method=RequestMethod.GET)
 	public String fileup() {
 		return "fileUp";
 	}
+	@RequestMapping(value="fileup", method=RequestMethod.POST)
+	public ModelAndView fileup(MultipartHttpServletRequest mRequest, 
+			ModelAndView mav) {
+		if(fService.fileUp(mRequest, mav)) {
+			
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- source/09_Spring/ch13_fileupload/src/main/webapp/WEB-INF/views/fileUp.jsp
+++ source/09_Spring/ch13_fileupload/src/main/webapp/WEB-INF/views/fileUp.jsp
@@ -12,11 +12,21 @@
 <body>
 	<div align="center">
 		<hr width="500" color="green">
-		<h2>파일 upload</h2>
+		<h2>파일 upload (fileUp.jsp)</h2>
 		<hr width="500" color="green">
+		<form action="fileup.do" method="post" enctype="multipart/form-data">
+			<p>첨부파일1 <input type="file" name="file1"></p>
+			<p>첨부파일2 <input type="file" name="file2"></p>
+			<p>첨부파일3 <input type="file" name="file3"></p>
+			<input type="submit" value="업로드">
+		</form>
 	</div>
 </body>
 </html>
+
+
+
+
 
 
 
