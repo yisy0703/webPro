6cef8ff1fdea7b7ed1158df844aa5287e445b4e9
yisy0703 <yisy0703@naver.com>
2023-04-10 11:50:50 +0900

    jdbcTemplate

--- source/09_Spring/ch12_jdbcTemplateBoard/src/main/java/com/lec/ch12/dao/BoardDao.java
+++ source/09_Spring/ch12_jdbcTemplateBoard/src/main/java/com/lec/ch12/dao/BoardDao.java
@@ -110,101 +110,21 @@ public class BoardDao {
 	}
 	// 4. bID로 조회수 1 올리기
 	private void hitUp(int bid) {
-		Connection        conn  = null;
-		PreparedStatement pstmt = null;
-		String sql = "UPDATE MVC_BOARD SET bHIT = bHIT + 1 WHERE bID = ?";
-		try {
-			conn = ds.getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, bid);
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println(e.getMessage() + " 조회수 up 실패");
-		} finally {
-			try {
-				if(pstmt != null) pstmt.close();
-				if(conn  != null) conn.close();
-			} catch (SQLException e) {
-				System.out.println(e.getMessage());
-			} 
-		}
+		String sql = "UPDATE MVC_BOARD SET bHIT = bHIT + 1 WHERE bID = " + bid;
+		template.update(sql);
 	}
 	// 5. bID로 DTO가져오기 (글상세보기) -- 조회수 올리고 dto가져오기
 	public BoardDto content(int bid) {
-		BoardDto dto = null;
-		hitUp(bid); // 글 상세보기 시 조회수 1 올리기
-		Connection        conn  = null;
-		PreparedStatement pstmt = null;
-		ResultSet         rs    = null;
-		String sql = "SELECT * FROM MVC_BOARD WHERE bID = ?";
-		try {
-			conn = ds.getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, bid);
-			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				String bname    = rs.getString("bname");
-				String btitle   = rs.getString("btitle");
-				String bcontent = rs.getString("bcontent");
-				Timestamp bdate = rs.getTimestamp("bdate");
-				int    bhit     = rs.getInt("bhit");
-				int    bgroup   = rs.getInt("bgroup");
-				int    bstep    = rs.getInt("bstep");
-				int    bindent  = rs.getInt("bindent");
-				String bip      = rs.getString("bip");
-				dto = new BoardDto(bid, bname, btitle, bcontent, bdate, bhit, 
-						bgroup, bstep, bindent, bip);
-			}
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		} finally {
-			try {
-				if(rs    != null) rs.close();
-				if(pstmt != null) pstmt.close();
-				if(conn  != null) conn.close();
-			} catch (SQLException e) {
-				System.out.println(e.getMessage());
-			} 
-		}
-		return dto;
+		hitUp(bid);
+		String sql = "SELECT * FROM MVC_BOARD WHERE bID = " + bid;
+		return template.queryForObject(sql, 
+				new BeanPropertyRowMapper<BoardDto>(BoardDto.class));
 	}
 	// 6. bID로 DTO가져오기 (글수정VIEW, 답변글VIEW) -- dto 가져오기
 	public BoardDto modifyView_replyView(int bid) {
-		BoardDto dto = null;
-		Connection        conn  = null;
-		PreparedStatement pstmt = null;
-		ResultSet         rs    = null;
-		String sql = "SELECT * FROM MVC_BOARD WHERE bID = ?";
-		try {
-			conn = ds.getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, bid);
-			rs = pstmt.executeQuery();
-			if(rs.next()) {
-				String bname    = rs.getString("bname");
-				String btitle   = rs.getString("btitle");
-				String bcontent = rs.getString("bcontent");
-				Timestamp bdate = rs.getTimestamp("bdate");
-				int    bhit     = rs.getInt("bhit");
-				int    bgroup   = rs.getInt("bgroup");
-				int    bstep    = rs.getInt("bstep");
-				int    bindent  = rs.getInt("bindent");
-				String bip      = rs.getString("bip");
-				dto = new BoardDto(bid, bname, btitle, bcontent, bdate, bhit, 
-						bgroup, bstep, bindent, bip);
-			}
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		} finally {
-			try {
-				if(rs    != null) rs.close();
-				if(pstmt != null) pstmt.close();
-				if(conn  != null) conn.close();
-			} catch (SQLException e) {
-				System.out.println(e.getMessage());
-			} 
-		}
-		return dto;
+		String sql = "SELECT * FROM MVC_BOARD WHERE bID = " + bid;
+		return template.queryForObject(sql, 
+				new BeanPropertyRowMapper<BoardDto>(BoardDto.class));
 	}
 	// 7. 글 수정 (특정 bid의 작성자, 글제목, 글본문, bip만 수정)
 	public int modify(int bid, String bname, String btitle, String bcontent, 
