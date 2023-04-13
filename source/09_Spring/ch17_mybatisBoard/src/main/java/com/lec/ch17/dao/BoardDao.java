ddc05bf893f1a6fe674e61938b7ac2db6f23d924
yisy0703 <yisy0703@naver.com>
2023-04-13 15:14:06 +0900

    mybatis

--- source/09_Spring/ch17_mybatisBoard/src/main/java/com/lec/ch17/dao/BoardDao.java
+++ source/09_Spring/ch17_mybatisBoard/src/main/java/com/lec/ch17/dao/BoardDao.java
@@ -1,8 +1,20 @@
 package com.lec.ch17.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.lec.ch17.vo.Board;
 
 @Mapper
 public interface BoardDao {
-
+	public List<Board> boardList(Board board);
+	public int boardTotCnt();
+	public int boardWrite(Board board);
+	public void boardHitUp(int bid);
+	public Board getBoardDto(int bid);
+	public int boardUpdate(Board board);
+	public int boardDelete(int bid);
+	public void boardReplyPreStep(Board board);
+	public int boardReply(Board board);
 }
