1e3e88185af2aedd436dfa168be8d1e1c8f6182a
yisy0703 <yisy0703@naver.com>
2023-04-14 14:12:50 +0900

    bookUpMember

---
+++ source/09_Spring/ch19_bookupMember/src/main/resources/META-INF/db/mappers.sql
@@ -0,0 +1,29 @@
+-- ★ ★ ★ ★ ★ book.xml ★ ★ ★ ★ ★ 
+-- id = mainList (신간도서순 bookList)
+
+-- id = bookList (startRow~endRow까지 bookList) 출력순서:bTITLE 기준
+
+-- id = totCntBook (등록된 책 갯수)
+
+-- id = getDetailBook (책번호로 dto가져오기)
+
+-- id = registerBook (책등록)
+
+-- id = modifyBook (책수정)
+
+-- ★ ★ ★ ★ ★ member.xml ★ ★ ★ ★ ★ 
+-- id = idConfirm (mid가 id인 데이터 갯수) : public int idConfirm(String mid);
+SELECT COUNT(*) FROM MEMBER WHERE MID='aaa';
+--	id = joinMember(회원가입)
+
+--	id = getDetailMember(mid로 Member dto가져오기)
+
+--	id = modifyMember(회원정보 수정);
+
+
+
+
+
+
+
+
