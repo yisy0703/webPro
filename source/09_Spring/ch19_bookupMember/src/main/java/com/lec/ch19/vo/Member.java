f40a54f02ff9f3bfa51f33455137db4dd39a9b8b
yisy0703 <yisy0703@naver.com>
2023-04-14 14:57:36 +0900

    bookUpMember

---
+++ source/09_Spring/ch19_bookupMember/src/main/java/com/lec/ch19/vo/Member.java
@@ -0,0 +1,10 @@
+package com.lec.ch19.vo;
+
+public class Member {
+	private String mid;
+	private String mpw;
+	private String mname;
+	private String mmail;
+	private String mpost;
+	private String maddr;
+}
