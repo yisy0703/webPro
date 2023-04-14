2dee4ae61f81ce6bc749073f233ab4f5881ce1f9
yisy0703 <yisy0703@naver.com>
2023-04-14 15:25:32 +0900

    bookUpMember

---
+++ source/09_Spring/ch19_bookupMember/src/main/java/com/lec/ch19/service/BookService.java
@@ -0,0 +1,14 @@
+package com.lec.ch19.service;
+import java.util.List;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import com.lec.ch19.vo.Book;
+public interface BookService {
+	public List<Book> mainList();
+	public List<Book> bookList(String pageNum);
+	public int totCntBook();
+	public Book getDetailBook(int bnum);
+	public int registerBook(MultipartHttpServletRequest mRequest, Book book);
+	public int registerBook(MultipartHttpServletRequest mRequest); // 비추
+	public int modifyBook(MultipartHttpServletRequest mRequest, Book book);
+	//public int modifyBook(MultipartHttpServletRequest mRequest);
+}
