afb5c23284dee09273dbe0780e39b2b076bd2572
yisy0703 <yisy0703@naver.com>
2023-04-28 10:16:16 +0900

    2ndTeamProject

--- source/10_2ndTeamProject/test06_categorySelect/src/main/java/com/tj/test06/dao/MemberDao.java
+++
@@ -1,7 +0,0 @@
-package com.tj.test06.dao;
-
-import com.tj.test06.dto.Member;
-
-public interface MemberDao {
-	public Member login(Member member);
-}
--- source/10_2ndTeamProject/test06_categorySelect/src/main/java/com/tj/test06/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.tj.test06.service;
-
-import javax.servlet.http.HttpSession;
-
-import com.tj.test06.dto.Member;
-
-public interface MemberService {
-	public Member login(Member member, HttpSession session);
-}
