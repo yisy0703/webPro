e8df24df16743c778fddf7ec26a9405b5b7bfbac
yisy0703 <yisy0703@naver.com>
2023-05-01 09:53:51 +0900

    2ndTeamProject

---
+++ temp/ch01_hello-maven/src/test/java/com/lec/hellomaven/HelloMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lec.hellomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
