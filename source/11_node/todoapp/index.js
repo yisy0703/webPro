52bc118477180b193d23b6287515d855cd536b0c
yisy0703 <yisy0703@naver.com>
2023-05-09 15:34:07 +0900

    node.js

--- source/11_node/todoapp/index.js
+++ source/11_node/todoapp/index.js
@@ -1,6 +1,8 @@
 const express = require('express'); // express 라이브러리 첨부
 const app = express(); // express라이브러리를 이용, 객체 생성
 
+const uri = "mongodb+srv://yisy0703:ftnwLKSfNEPRGMS5@cluster0.rjo9xgd.mongodb.net/?retryWrites=true&w=majority";
+
 app.listen(8090, function(){
   console.log("listening on 8090");
 });
@@ -15,3 +17,16 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.static('public')); // css, js 등의 static 파일은 public 폴더
+
+// '/write' 요청경로(get)가 들어왔을 때 write.html로 가라
+app.get('/write', (req, res) => {
+  res.sendFile(__dirname + '/write.html');
+});
+// '/write' 요청경로(post)가 들어왔을 때, 파라미터 정보(title, date)를 DB에 저장
+app.use(express.urlencoded({extended : true})); // post방식으로 들어온 req의 파라미터 사용
+app.post('/write', (req, res) => {
+  console.log(req.body.title);
+  console.log(req.body.date);
+  console.log(req.body); // josn 형태
+  res.sendFile(__dirname + '/write.html');
+});
